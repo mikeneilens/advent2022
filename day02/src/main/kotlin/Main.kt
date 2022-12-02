9cec50086aaff39d84fedecb6b8871e96cfe4f27
Mike Neilens <mike.neilens@gmail.com>
2022-12-02 10:36:57 +0000

    day 02

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,1 +1,40 @@
-fun helloWorld() = "hello world"
+enum class Item(val resultValue: Int) {
+    Rock(1), Paper(2), Scissors(3)
+}
+
+val itemMap = mapOf("A" to Item.Rock, "B" to Item.Paper, "C" to Item.Scissors, "X" to Item.Rock, "Y" to Item.Paper, "Z" to Item.Scissors)
+
+val winMap = mapOf( Item.Rock to Item.Scissors, Item.Scissors to Item.Paper, Item.Paper to Item.Rock )
+val loseMap = winMap.toList().associate { Pair(it.second, it.first) }
+
+data class Round(val theirs: Item, val yours: Item) {
+    fun yourScore() = yours.resultValue +
+        if ( winMap[yours] == theirs)  6
+        else if ( theirs == yours) 3
+        else 0
+}
+
+fun parseInput(data: List<String>, lineParser: (String)->Round ) = data.map(lineParser)
+
+fun parseLine(line: String ) =
+   Round(itemMap.getValue(line.split(" ")[0]), itemMap.getValue(line.split(" ")[1]))
+
+fun List<Round>.totalScore() = sumOf(Round::yourScore)
+
+fun partOne(data: List<String>) = parseInput(data, ::parseLine).totalScore()
+
+//part two
+
+fun parseLine2(line: String):Round {
+    val theirs = itemMap.getValue(line.split(" ")[0])
+    val instruction = line.split(" ")[1]
+    return Round(theirs, calculateYours(instruction, theirs))
+}
+
+fun calculateYours(instruction: String, theirs: Item) = when (instruction) {
+        "X" -> winMap.getValue(theirs)
+        "Z" -> loseMap.getValue(theirs)
+        else -> theirs
+    }
+
+fun partTwo(data:List<String>) = parseInput(data, ::parseLine2).totalScore()
--- day02/src/test/kotlin/MainTest.kt
+++ day02/src/test/kotlin/MainTest.kt
@@ -1,15 +1,44 @@
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
+
 class MainTest: WordSpec( {
+
+    val sampleInput = """
+        A Y
+        B X
+        C Z
+    """.trimIndent().split("\n")
+
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "give correct round data when parsing a line on the input" {
+            parseLine(sampleInput[0]) shouldBe Round(Item.Rock,Item.Paper)
+            parseLine(sampleInput[1]) shouldBe Round(Item.Paper,Item.Rock)
+            parseLine(sampleInput[2]) shouldBe Round(Item.Scissors,Item.Scissors)
+        }
+        "give the correct score for each line on the sample input" {
+            parseLine(sampleInput[0]).yourScore() shouldBe 8
+            parseLine(sampleInput[1]).yourScore() shouldBe 1
+            parseLine(sampleInput[2]).yourScore() shouldBe 6
+        }
+        "part one with sample input gives score of 15" {
+            partOne(sampleInput) shouldBe 15
+        }
+        "part one with puzzle input gives score of 10624" {
+            partOne(puzzleInput) shouldBe 10624
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "give correct round data when parsing a line on the input for part two" {
+            parseLine2(sampleInput[0]) shouldBe Round(Item.Rock,Item.Rock)
+            parseLine2(sampleInput[1]) shouldBe Round(Item.Paper,Item.Rock)
+            parseLine2(sampleInput[2]) shouldBe Round(Item.Scissors,Item.Rock)
+        }
+        "part two with sample input gives score of 12" {
+            partTwo(sampleInput) shouldBe 12
+        }
+        "part two with puzzle input gives score of 14060" {
+            partTwo(puzzleInput) shouldBe 14060
         }
     })
 })
