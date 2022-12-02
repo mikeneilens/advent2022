2ad6df4fbe5a34c061b9f8e86af10471a0612633
Mike Neilens <mike.neilens@gmail.com>
2022-12-02 11:41:40 +0000

    day02

--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -5,7 +5,6 @@ enum class Item(val resultValue: Int) {
 val itemMap = mapOf("A" to Item.Rock, "B" to Item.Paper, "C" to Item.Scissors, "X" to Item.Rock, "Y" to Item.Paper, "Z" to Item.Scissors)
 
 val winMap = mapOf( Item.Rock to Item.Scissors, Item.Scissors to Item.Paper, Item.Paper to Item.Rock )
-val loseMap = winMap.toList().associate { Pair(it.second, it.first) }
 
 data class Round(val theirs: Item, val yours: Item) {
     fun yourScore() = yours.resultValue +
@@ -25,16 +24,20 @@ fun partOne(data: List<String>) = parseInput(data, ::parseLine).totalScore()
 
 //part two
 
+val translateTheirToYours = mapOf(
+    Pair("X", Item.Rock) to Item.Scissors,
+    Pair("X", Item.Paper) to Item.Rock,
+    Pair("X", Item.Scissors) to Item.Paper,
+    Pair("Z", Item.Rock) to Item.Paper,
+    Pair("Z", Item.Paper) to Item.Scissors,
+    Pair("Z", Item.Scissors) to Item.Rock)
+
 fun parseLine2(line: String):Round {
     val theirs = itemMap.getValue(line.split(" ")[0])
     val instruction = line.split(" ")[1]
     return Round(theirs, calculateYours(instruction, theirs))
 }
 
-fun calculateYours(instruction: String, theirs: Item) = when (instruction) {
-        "X" -> winMap.getValue(theirs)
-        "Z" -> loseMap.getValue(theirs)
-        else -> theirs
-    }
+fun calculateYours(instruction: String, theirs: Item) = translateTheirToYours[Pair(instruction,theirs)] ?: theirs
 
 fun partTwo(data:List<String>) = parseInput(data, ::parseLine2).totalScore()
