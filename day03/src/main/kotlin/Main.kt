1de4abcfe21005b33438b46eebb85cafa830f5db
Mike Neilens <mike.neilens@gmail.com>
2022-12-03 06:09:05 +0000

    day03

--- day03/src/main/kotlin/Main.kt
+++ day03/src/main/kotlin/Main.kt
@@ -1,1 +1,26 @@
-fun helloWorld() = "hello world"
+data class Rucksack(val left:List<Char>, val right:List<Char>, val all:List<Char> = left + right) {
+    fun findFirstInBothSides() = left.first{ right.contains(it)}
+    fun findCodeForFirstInBothSides() = findFirstInBothSides().toValue()
+}
+
+fun String.toRucksack() = Rucksack(firstHalf(), secondHalf())
+
+fun String.firstHalf() = toList().take(length/2)
+
+fun String.secondHalf() = toList().drop(length/2)
+
+fun Char.toValue() = if (this >= 'a') code - 96 else code - 38
+
+fun partOne(data:List<String>) = data.map(String::toRucksack).sumOf(Rucksack::findCodeForFirstInBothSides)
+
+//part two
+
+data class ElvesBags(val ruckSacks:List<Rucksack>) {
+    fun findBadge() = ruckSacks.first().all.first{ ruckSacks[1].all.contains(it) && ruckSacks[2].all.contains(it)}
+    fun findCodeForBadge() = findBadge().toValue()
+}
+
+fun List<String>.toElvesBags() = map(String::toRucksack).chunked(3).map(::ElvesBags)
+
+fun partTwo(data:List<String>) = data.toElvesBags().sumOf( ElvesBags::findCodeForBadge )
+
--- day03/src/test/kotlin/MainTest.kt
+++ day03/src/test/kotlin/MainTest.kt
@@ -2,14 +2,66 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+
+    val sampleInput = """
+        vJrwpWtwJgWrhcsFMMfFFhFp
+        jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        PmmdzqPrVvPwwTWBwg
+        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        ttgJtRGJQctTZtZT
+        CrZsJsPPZsGzwwsLwLmpwMDw
+    """.trimIndent().split("\n")
+
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "convert first half of abcd is [a,b]" {
+            "abcd".firstHalf() shouldBe listOf('a','b')
+        }
+        "convert second half of abcd is [c,d]" {
+            "abcd".secondHalf() shouldBe listOf('c','d')
+        }
+        "parse abcd into Rucksack([a,b],[c,d])" {
+            "abcd".toRucksack() shouldBe Rucksack(listOf('a','b'), listOf('c','d'),listOf('a','b','c','d'))
+        }
+        "find first item in vJrwpWtwJgWrhcsFMMfFFhFp contains in both sides is p" {
+            val rucksack = "vJrwpWtwJgWrhcsFMMfFFhFp".toRucksack()
+            rucksack.findFirstInBothSides() shouldBe  'p'
+        }
+        "find value of first item in vJrwpWtwJgWrhcsFMMfFFhFp contains in both sides is p" {
+            val rucksack = "vJrwpWtwJgWrhcsFMMfFFhFp".toRucksack()
+            rucksack.findCodeForFirstInBothSides() shouldBe  16
+        }
+        "convert a to value of 1" {
+            'a'.toValue() shouldBe 1
+        }
+        "convert A to value of 27" {
+            'A'.toValue() shouldBe 27
+        }
+        "part one with sample input is 157" {
+            partOne(sampleInput) shouldBe 157
+        }
+        "part one with puzzle input is 7763" {
+            partOne(puzzleInput) shouldBe 7763
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "find badge in first 3 bags of puzzle input is r"{
+            val elvesBags = sampleInput.take(3).toElvesBags().first()
+            elvesBags.findBadge() shouldBe 'r'
         }
+        "find badge in second 3 bags of puzzle input is Z"{
+            val elvesBags = sampleInput.drop(3).toElvesBags().first()
+            elvesBags.findBadge() shouldBe 'Z'
+        }
+        "find code of badge in first 3 bags of puzzle input is r"{
+            val elvesBags = sampleInput.take(3).toElvesBags().first()
+            elvesBags.findCodeForBadge() shouldBe 18
+        }
+        "part two with sample input is 70" {
+            partTwo(sampleInput) shouldBe 70
+        }
+        "part two with sample input is 2569" {
+            partTwo(puzzleInput) shouldBe 2569
+        }
+
     })
 })
