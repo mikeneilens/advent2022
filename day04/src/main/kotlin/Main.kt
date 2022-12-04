89f814b687ac567a3332841eb2a6f153981d1644
Mike Neilens <mike.neilens@gmail.com>
2022-12-04 07:03:29 +0000

    day04

--- day04/src/main/kotlin/Main.kt
+++ day04/src/main/kotlin/Main.kt
@@ -1,1 +1,28 @@
-fun helloWorld() = "hello world"
+data class Assignment (val start:Int, val end:Int ) {
+    fun insideOther(other: Assignment) = start <= other.start && end >= other.end
+}
+
+data class Section(val first:Assignment, val second:Assignment) {
+    fun assignmentContainsOtherAssignment() =
+        first.insideOther(second) || second.insideOther(first)
+
+    fun assignmentsOverlap():Boolean {
+        val section = getAssignmentsInOrder()
+        return (section.second.start <= section.first.end)
+    }
+
+    fun getAssignmentsInOrder():Section =
+        if (first.start <= second.start) this else Section(second, first)
+}
+
+fun String.toSection():Section {
+    val firstAssignment = getAssignment(0)
+    val secondAssignment = getAssignment(1)
+    return Section(Assignment(firstAssignment.first(), firstAssignment.last()), Assignment(secondAssignment.first(), secondAssignment.last()) )
+}
+
+fun String.getAssignment(n: Int) = split(",")[n].split("-").map{it.toInt()}
+
+fun partOne(data: List<String>) = data.map(String::toSection).count(Section::assignmentContainsOtherAssignment)
+
+fun partTwo(data:List<String>) = data.map(String::toSection).count(Section::assignmentsOverlap)
--- day04/src/test/kotlin/MainTest.kt
+++ day04/src/test/kotlin/MainTest.kt
@@ -2,14 +2,28 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "sampleInput with part one contains two assignments with one inside the other" {
+            partOne(sampleInput) shouldBe 2
+        }
+        "puzzleInput with part one contains 571 assignments with one inside the other" {
+            partOne(puzzleInput) shouldBe 571
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "sampleInput with part two contains 4 pairs with one inside the other" {
+            partTwo(sampleInput) shouldBe 4
+        }
+        "puzzleInput with part two contains 917 pairs with one inside the other" {
+            partTwo(puzzleInput) shouldBe 917
         }
     })
 })
