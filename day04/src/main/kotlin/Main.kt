2c43a537bba7edbb37cda6057e6d8e13154346dc
Mike Neilens <mike.neilens@gmail.com>
2022-12-04 07:10:01 +0000

    day04

--- day04/src/main/kotlin/Main.kt
+++ day04/src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-data class Assignment (val start:Int, val end:Int ) {
+data class Assignment (val start: Int, val end: Int ) {
     fun insideOther(other: Assignment) = start <= other.start && end >= other.end
 }
 
-data class Section(val first:Assignment, val second:Assignment) {
+data class Section(val first: Assignment, val second: Assignment) {
     fun assignmentContainsOtherAssignment() = first.insideOther(second) || second.insideOther(first)
 
     fun assignmentsOverlap():Boolean {
@@ -10,18 +10,14 @@ data class Section(val first:Assignment, val second:Assignment) {
         return (section.second.start <= section.first.end)
     }
 
-    fun getAssignmentsInOrder():Section =
-        if (first.start <= second.start) this else Section(second, first)
+    fun getAssignmentsInOrder() = if (first.start <= second.start) this else Section(second, first)
 }
 
 fun String.toSection():Section {
-    val firstAssignment = getAssignment(0)
-    val secondAssignment = getAssignment(1)
-    return Section(Assignment(firstAssignment.first(), firstAssignment.last()), Assignment(secondAssignment.first(), secondAssignment.last()) )
+    val values = split(",","-").map(String::toInt)
+    return Section(Assignment(values[0], values[1]), Assignment(values[2], values[3]) )
 }
-
-fun String.getAssignment(n: Int) = split(",")[n].split("-").map{it.toInt()}
 
 fun partOne(data: List<String>) = data.map(String::toSection).count(Section::assignmentContainsOtherAssignment)
 
-fun partTwo(data:List<String>) = data.map(String::toSection).count(Section::assignmentsOverlap)
+fun partTwo(data: List<String>) = data.map(String::toSection).count(Section::assignmentsOverlap)
