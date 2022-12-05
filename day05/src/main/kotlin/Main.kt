a3a0a2c0173c66bacdb37bfea6f3bddbef6b7597
Mike Neilens <mike.neilens@gmail.com>
2022-12-05 15:36:10 +0000

    day05 needs some refactoring

--- day05/src/main/kotlin/Main.kt
+++ day05/src/main/kotlin/Main.kt
@@ -1,1 +1,53 @@
 fun helloWorld() = "hello world"
+
+typealias Stack = List<Char>
+typealias Stacks = List<Stack>
+
+data class Command(val qty:Int, val from:Int, val to:Int)
+
+fun String.addToStacks(stacks: Stacks):Stacks =
+     stacks.mapIndexed{ index, stack -> addLineToStack(index, stack, this)}
+
+fun addLineToStack(index: Int, stack: Stack, line: String):Stack  {
+    val char = line.get(index * 4 + 1)
+    return if (char != ' ') stack + char else stack
+}
+
+fun initialiseStacks(data: List<String>):Stacks {
+    val emptyStacks:Stacks = listOf<Stack>(listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf()).take(data.noOfStacks())
+    val lines = data.filter { it.startsWith(' ') || it.startsWith('[') }.reversed().drop(1)
+    return lines.fold(emptyStacks){ stacks, line ->  line.addToStacks(stacks)}
+}
+
+fun List<String>.noOfStacks() = first{it.startsWith(" 1")}.trim().split("   ").map(String::toInt).last()
+
+fun parseIntoCommands(data: List<String>):List<Command> = data.filter{it.startsWith("move")}
+    .map {line ->
+        val components = line.removePrefix("move ").split(" from ", " to ").map(String::toInt)
+        Command(components[0],components[1],components[2])
+    }
+
+fun partOne(data:List<String>):String {
+    val commands = parseIntoCommands(data)
+    val stacks = initialiseStacks(data)
+    return commands.process(stacks, Stacks::mover).map{it.last()}.joinToString("")
+}
+
+fun List<Command>.process(stacks: Stacks, mover: Stacks.(Command)-> Stacks) = fold(stacks){ stacks, command -> stacks.mover(command)}
+
+fun Stacks.mover(command: Command):Stacks = mapIndexed { index, stack -> move(index, stack,command) }
+
+fun Stacks.move(index:Int, stack:Stack, command: Command, shouldReverse:Boolean = true) =  when(true) {
+    (command.from - 1 == index  ) -> stack.dropLast(command.qty)
+    (command.to -1 == index && shouldReverse ) -> stack + this[command.from -1].takeLast(command.qty).reversed()
+    (command.to -1 == index && !shouldReverse ) -> stack + this[command.from -1].takeLast(command.qty)
+    else -> stack
+}
+
+fun partTwo(data:List<String>):String {
+    val commands = parseIntoCommands(data)
+    val stacks = initialiseStacks(data)
+    return commands.process(stacks, Stacks::mover2).map{it.last()}.joinToString("")
+}
+
+fun Stacks.mover2(command: Command):Stacks = mapIndexed { index, stack -> move(index, stack,command, false) }
--- day05/src/test/kotlin/MainTest.kt
+++ day05/src/test/kotlin/MainTest.kt
@@ -2,14 +2,66 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+
+    val sampleData = """
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "add a line of text to a list of stacks" {
+            val line = "[G] [L] [M] [S] [S] [C]     [T] [V]"
+            val stacks:Stacks = listOf(listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf(),listOf())
+            line.addToStacks(stacks) shouldBe listOf(listOf('G'),listOf('L'),listOf('M'),listOf('S'),listOf('S'),listOf('C'),listOf(),listOf('T'),listOf('V'))
         }
+        "calculate no of stacks on sample data is 3" {
+            sampleData.noOfStacks() shouldBe 3
+        }
+        "initialise stacks from puzzle input" {
+            initialiseStacks(puzzleInput)[0] shouldBe listOf('R','N','P','G')
+            initialiseStacks(puzzleInput)[8] shouldBe listOf('L','P','B','V','M','J','F')
+        }
+        "parse sample data into commands" {
+            val commands = parseIntoCommands(sampleData)
+            commands.size shouldBe 4
+            commands[0] shouldBe Command(1,2,1)
+            commands[1] shouldBe Command(3,1,3)
+            commands[2] shouldBe Command(2,2,1)
+            commands[3] shouldBe Command(1,1,2)
+        }
+        "one crate is moved from stack 2 to stack 1" {
+            val stacks = initialiseStacks(sampleData)
+            val command = Command(1, 2, 1)
+            val newStacks  = stacks.mover(command)
+            newStacks[0] shouldBe listOf('Z','N','D')
+            newStacks[1] shouldBe listOf('M','C')
+            newStacks[2] shouldBe listOf('P')
+        }
+        "process all stacks for sample data should be CMZ" {
+            val stacks = listOf(listOf('Z', 'N'), listOf('M', 'C', 'D'), listOf('P'))
+            val commands = listOf(Command(qty=1, from=2, to=1), Command(qty=3, from=1, to=3), Command(qty=2, from=2, to=1), Command(qty=1, from=1, to=2))
+            commands.process(stacks, Stacks::mover) shouldBe listOf(listOf('C'),listOf('M'), listOf('P', 'D', 'N', 'Z'))
+        }
+        "answer for part one using sample data is CMZ " {
+            partOne(sampleData) shouldBe "CMZ"
+        }
+        "answer for part one using puzzle input is HBTMTBSDC " {
+            partOne(puzzleInput) shouldBe "HBTMTBSDC"
+        }
+
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "answer for part two using sample data is MCD " {
+            partTwo(sampleData) shouldBe "MCD"
+        }
+        "answer for part two using puzzle input is PQTJRSHWS " {
+            partTwo(puzzleInput) shouldBe "PQTJRSHWS"
         }
     })
 })
