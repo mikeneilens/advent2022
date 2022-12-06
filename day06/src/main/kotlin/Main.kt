053887509b279cb96be0117b53756b49f6a16de0
Mike Neilens <mike.neilens@gmail.com>
2022-12-06 09:00:18 +0000

    day06

--- day06/src/main/kotlin/Main.kt
+++ day06/src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
 
 fun String.findFirstMarker(size: Int = 4) =
-    windowed(size,1).indexOfFirst { it.toSet().toList() == it.toList() } + size
+    windowed(size,1).indexOfFirst { it.toSet().size == size } + size
