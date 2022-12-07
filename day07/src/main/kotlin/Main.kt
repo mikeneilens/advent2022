981b63c5ebcc9e13153f11c7d7e615331a76f10a
Mike Neilens <mike.neilens@gmail.com>
2022-12-07 19:13:37 +0000

    day07 refactored

--- day07/src/main/kotlin/Main.kt
+++ day07/src/main/kotlin/Main.kt
@@ -1,60 +1,65 @@
-fun helloWorld() = "hello world"
-
 data class Directory(val name:String, val parent:Directory?, val files:MutableList<File>, val children:MutableList<Directory>) {
 
     val size:Int by lazy { files.sumOf { it.size } + children.sumOf { it.size } }
 
-    fun sizeSmallerThan(max:Int):Int = allDirectories().filter{it.size <= max}.sumOf { it.size }
-
-    fun directoryToDelete(spaceNeeded: Int) = allDirectories().filter{it.size >= spaceNeeded}.sortedBy { it.size }.first()
-
     fun allDirectories():List<Directory> = children.flatMap { directory -> directory.allDirectories()} + this
 
-    override fun toString(): String {
-        return "name=$name, files=${files.size} children=${children.size}"
+    fun totalSizeOfDirectoriesSmallerThan(max:Int) = allDirectories().filter{it.size <= max}.sumOf { it.size }
+
+    fun directoryToDelete(spaceNeeded: Int) = allDirectories().filter { it.size >= spaceNeeded }.minByOrNull { it.size }!!
+
+    fun addChildDirectory(name: String):Directory {
+        if (children.none{ it.name == name }) {
+            children.add(Directory(name, this, mutableListOf(), mutableListOf()))
+        }
+        return this
     }
+    fun addFile(fileName: String, fileSize: Int):Directory {
+        files.add(File(fileName, fileSize))
+        return this
+    }
+
+    override fun toString() = "name=$name, files=${files.size} children=${children.size}"
 }
 
 data class File(val name:String, val size:Int)
 
-fun partOne(data:List<String>):Int {
-    return parseIntoDirectory(data).sizeSmallerThan(100000)
+typealias Processor = (line: String, directory: Directory, root:Directory) -> Directory
+
+val changeToRoot:Processor = { _, _, root -> root}
+
+val moveOutOneLevel:Processor = {_, directory, root -> directory.parent ?: root}
+
+val listDirectory:Processor = {_, directory,_ -> directory}
+
+val addFileToDirectory:Processor = { line: String, directory: Directory, _:Directory -> directory.addFile(line.fileName(), line.fileSize()) }
+
+val addDirectory:Processor = { line: String, directory: Directory, _:Directory -> directory.addChildDirectory(line.directoryName()) }
+
+val changeDirectory:Processor  = { line: String, directory: Directory, _:Directory ->
+    directory.addChildDirectory(line.directoryName())
+    directory.children.first { it.name == line.directoryName() }
 }
+
+val processInstruction = mapOf(
+    """^\$ cd /""".toRegex() to changeToRoot,
+    """^\$ cd ..""".toRegex() to moveOutOneLevel,
+    """^\$ cd [0-9A-Za-z.]+""".toRegex() to changeDirectory,
+    """^\$ ls""".toRegex() to listDirectory,
+    """^dir [0-9A-Za-z.]+""".toRegex() to addDirectory
+)
 
 fun parseIntoDirectory(data:List<String>):Directory {
     val root = Directory("root", null, mutableListOf(), mutableListOf())
-    var currentDirectory = root
-    var directoryBeingListed = root
-    data.forEach{ line ->
-        when(true) {
-            (line.startsWith("$ cd /")) -> currentDirectory = root
-            (line.startsWith("$ cd ..")) -> currentDirectory = currentDirectory.parent ?: root
-            (line.startsWith("$ cd")) -> {
-                val directoryName = line.split(" ")[2]
-                if (currentDirectory.children.none { it.name == directoryName }) {
-                    currentDirectory.children.add(
-                        Directory(directoryName, parent = currentDirectory, mutableListOf(), mutableListOf())
-                    )
-                }
-                currentDirectory  = currentDirectory.children.first { it.name == directoryName }
-            }
-            (line.startsWith("dir")) -> {
-                val listDirectoryName = line.split(" ").last()
-                if (currentDirectory.children.none { it.name == listDirectoryName }) {
-                    currentDirectory.children.add(
-                        Directory(listDirectoryName, parent = currentDirectory, mutableListOf(), mutableListOf())
-                    )
-                }
-            }
-            (line.startsWith("$ ls")) -> directoryBeingListed = currentDirectory
-            else -> {
-                val size = line.split(" ")[0].toInt()
-                val fileName = line.split(" ")[1]
-                directoryBeingListed.files.add(File(fileName, size))
-            }
-        }
+    data.fold(root) { directory, line ->
+        val processor = processInstruction.filter{ it.key matches line }.values.firstOrNull() ?: addFileToDirectory
+        processor(line, directory, root)
     }
     return root
+}
+
+fun partOne(data:List<String>):Int {
+    return parseIntoDirectory(data).totalSizeOfDirectoriesSmallerThan(100000)
 }
 
 fun partTwo(data:List<String>):Directory {
@@ -62,3 +67,7 @@ fun partTwo(data:List<String>):Directory {
     val spaceNeeded = directory.size - 40000000
     return directory.directoryToDelete(spaceNeeded)
 }
+
+fun String.fileSize() = split(" ")[0].toInt()
+fun String.fileName() = split(" ")[1]
+fun String.directoryName() = split(" ").last()
--- day07/src/test/kotlin/MainTest.kt
+++ day07/src/test/kotlin/MainTest.kt
@@ -27,7 +27,28 @@ class MainTest: WordSpec( {
         5626152 d.ext
         7214296 k
     """.trimIndent().split("\n")
+
+    val root = Directory("root", files = mutableListOf(File("f1",1)), children = mutableListOf(), parent = null)
+    val d1 = Directory("d1", files = mutableListOf(File("f2",2)), children = mutableListOf(), parent = root)
+    root.children.add(d1)
+    val d2 = Directory("d2", files = mutableListOf(File("f3",3)), children = mutableListOf(), parent = d1)
+    d1.children.add(d2)
+    val d3 = Directory("d3", files = mutableListOf(File("f4",4)), children = mutableListOf(), parent = d1)
+    d1.children.add(d3)
+
     "Part one" should ({
+        "return all directories correctly" {
+            root.allDirectories() shouldBe listOf(d2,d3,d1,root)
+        }
+        "return size of directories correctly" {
+            root.size shouldBe 10
+            d1.size shouldBe 9
+            d2.size shouldBe 3
+            d3.size shouldBe 4
+        }
+        "return directories below a certain size correctly" {
+            root.totalSizeOfDirectoriesSmallerThan(4) shouldBe 7
+        }
         "partOne using sampleInput" {
             partOne(sampleInput) shouldBe 95437
         }
@@ -36,6 +57,12 @@ class MainTest: WordSpec( {
         }
     })
     "Part two" should ({
+        "directory to delete to remove 8 should be d1 which has a size of 8" {
+            root.directoryToDelete(8) shouldBe d1
+        }
+        "directory to delete to remove 2 should be d2 which has a size of 3" {
+            root.directoryToDelete(2) shouldBe d2
+        }
         "partTwo using sampleInput should free up 24933642" {
             partTwo(sampleInput).size shouldBe  24933642
         }
