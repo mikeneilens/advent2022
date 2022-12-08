edf9009f2ef3a4760e42d7ff350ca33df992372f
Mike Neilens <mike.neilens@gmail.com>
2022-12-08 21:18:16 +0000

    day08

--- day08/src/main/kotlin/Main.kt
+++ day08/src/main/kotlin/Main.kt
@@ -1,1 +1,77 @@
 fun helloWorld() = "hello world"
+
+
+fun String.highestOnLine() = fold(listOf<Char>()) {
+        total, char -> if (total.size == 0 || char > total.last()) total + char else total + total.last()
+}
+
+fun List<String>.toColumns(size:Int):List<String> {
+    val columns = (1..size).map{mutableListOf<Char>()}
+    forEachIndexed {row, line ->
+        line.forEachIndexed { col, char ->
+            columns[col].add(char)
+        }
+    }
+    return columns.map{it.joinToString("")}
+}
+
+data class HeightMap(val rows:List<String>, val size:Int = rows.size) {
+    val columns = rows.toColumns(size)
+    val highestFromLeft:List<List<Char>> by lazy { rows.map{it.highestOnLine()}}
+    val highestFromRight by lazy { rows.map { it.reversed().highestOnLine() } }
+    val highestFromTop by lazy { columns.map{it.highestOnLine()}}
+    val highestFromBottom by lazy  {columns.map{it.reversed().highestOnLine()}}
+}
+
+fun isVisible(treeSize: Char, row: Int, col: Int, hm: HeightMap) =
+    (isHighest(treeSize, hm.highestFromLeft[row], col)
+            || isHighest(treeSize, hm.highestFromRight[row], hm.size - col - 1)
+            || isHighest(treeSize, hm.highestFromTop[col], row)
+            || isHighest(treeSize, hm.highestFromBottom[col], hm.size - row - 1)
+    )
+
+fun isHighest(treeSize: Char, row:List<Char>, col:Int) = ( treeSize == row[col] && (col == 0 || treeSize != row[col - 1]  )   )
+
+fun partOne(rows: List<String>):Int {
+    val treeMap = HeightMap(rows)
+    return (0 until treeMap.size).sumOf { row ->
+        (0 until treeMap.size).count { col ->
+            isVisible(rows[row][col], row, col, treeMap)
+        }
+    }
+}
+
+fun partTwo(rows: List<String>):Int {
+    val treeTopViews = createTreeTopViews(rows)
+    return (0 until rows.size).flatMap { row ->
+        (0 until rows.size).map { col -> treeTopViews.viewFrom(row, col) }
+    }.max()
+}
+
+fun String.viewFromEachTreeLeft() = mapIndexed{index, it -> treesVisibleBefore(index)}
+
+fun String.treesVisibleBefore(index:Int):Int {
+    if (index == 0) return 0
+    val char = get(index).toString()
+    var count = 1
+    while (index - count >= 0 && count < index && get(index - count).toString() < char &&(get(index - count).toString() < char))  {
+        count++
+    }
+    return count
+}
+
+fun createTreeTopViews(rows: List<String>):TreeTopViews {
+    val columns= rows.toColumns(rows.size)
+    return TreeTopViews(
+        rows.map(String::viewFromEachTreeLeft),
+        rows.map { it.reversed() }.map(String::viewFromEachTreeLeft),
+        columns.map(String::viewFromEachTreeLeft),
+        columns.map { it.reversed() }.map(String::viewFromEachTreeLeft),
+        rows.size
+    )
+}
+
+data class TreeTopViews(val viewFromLeft: List<List<Int>>, val viewFromRight: List<List<Int>>, val viewDown: List<List<Int>>, val viewUp: List<List<Int>>, val size:Int) {
+    fun viewFrom(row:Int, col:Int ):Int =
+         viewFromLeft[row][col] * viewFromRight[row][size - col - 1] * viewDown[col][row] * viewUp[col][size - row -1 ]
+}
--- day08/src/test/kotlin/MainTest.kt
+++ day08/src/test/kotlin/MainTest.kt
@@ -2,14 +2,94 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        30373
+        25512
+        65332
+        33549
+        35390
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "highest on line of 33549 is 33559" {
+            "33549".highestOnLine() shouldBe listOf('3','3','5','5','9')
         }
+        "sample Input columns are 32633, 05535, 35353, 71349, 32290" {
+            sampleInput.toColumns(sampleInput.first().length) shouldBe listOf("32633", "05535", "35353", "71349", "32290")
+        }
+        "[0][1] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('0',0,1,treeMap ) shouldBe true
+        }
+        "[2][0] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('6',2,0,treeMap ) shouldBe true
+        }
+        "[1][1] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('5',1,1,treeMap ) shouldBe true
+        }
+       "[1][2] in sample data is visible" {
+           val treeMap = HeightMap(sampleInput)
+            isVisible('5',1,2,treeMap ) shouldBe true
+        }
+        "[1][3] in sample data is not visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('1',1,3,treeMap ) shouldBe false
+        }
+        "[2][1] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('5',2,1,treeMap ) shouldBe true
+        }
+        "[2][2] in sample data is not visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('3',2,2,treeMap ) shouldBe false
+        }
+        "[2][3] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('3',2,3,treeMap ) shouldBe true
+        }
+        "[3][2] in sample data is visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('5',3,2,treeMap ) shouldBe true
+        }
+        "[3][1] in sample data is not visible" {
+            val treeMap = HeightMap(sampleInput)
+            isVisible('3',3,1,treeMap ) shouldBe false
+        }
+       "[3][3] in sample data is not visible" {
+           val treeMap = HeightMap(sampleInput)
+            isVisible('4',3,3,treeMap ) shouldBe false
+        }
+        "part one with sample data is 21" {
+            partOne(sampleInput) shouldBe 21
+        }
+        "part one with puzzle input is 1807" {
+            partOne(puzzleInput) shouldBe 1807
+        }
+
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "view from each tree when trees are 634545 is 0,1,2,3,1,2" {
+            "634545".viewFromEachTreeLeft() shouldBe listOf(0,1,2,3,1,2)
+        }
+        "scenic score for [1,2] in the sampleData is 4" {
+            val treeTopViews = createTreeTopViews(sampleInput)
+            treeTopViews.viewFrom(1,2) shouldBe 4
+        }
+        "scenic score for [3,2] in the sampleData is 8" {
+            val treeTopViews = createTreeTopViews(sampleInput)
+            treeTopViews.viewFrom(3,2) shouldBe 8
+        }
+        "part two with sample data is 8 " {
+            partTwo(sampleInput) shouldBe 8
+        }
+        "part two with puzzle input is " {
+            partTwo(puzzleInput) shouldBe 480000
+        }
+        "check1" {
+            val treeTopViews = createTreeTopViews(puzzleInput)
+            val left = puzzleInput.first().viewFromEachTreeLeft()
+            treeTopViews.viewFromLeft.first() shouldBe left
         }
     })
 })
