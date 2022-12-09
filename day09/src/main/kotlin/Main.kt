28d3779b050190c49e7dac08c4e861c5fe4952f1
Mike Neilens <mike.neilens@gmail.com>
2022-12-09 10:44:25 +0000

    day09

--- day09/src/main/kotlin/Main.kt
+++ day09/src/main/kotlin/Main.kt
@@ -1,1 +1,92 @@
+import javafx.geometry.Pos
+
 fun helloWorld() = "hello world"
+
+enum class Direction (val offset:Position) {
+    Right(Position(1,0)),
+    Left(Position(-1,0)),
+    Up(Position(0,-1)),
+    Down(Position(0,1)),
+    UpRight(Position(1,-1)),
+    UpLeft(Position(-1,-1)),
+    DownRight(Position(1,1)),
+    DownLeft(Position(-1,1)),
+    None(Position(0,0))
+}
+
+data class Instruction(val direction:Direction, val qty:Int)
+
+fun String.toInstruction():Instruction {
+    val direction = when (split(" ")[0]) {
+        "L" -> Direction.Left
+        "R" -> Direction.Right
+        "U" -> Direction.Up
+        "D" -> Direction.Down
+        else -> Direction.None
+    }
+    val qty = split(" ")[1].toInt()
+    return Instruction(direction,qty)
+}
+
+fun List<String>.parse() = map(String::toInstruction)
+
+data class Position(val x:Int, val y:Int) {
+    infix fun isTouching(other:Position):Boolean = (((x - other.x).abs) <= 1 && ((y - other.y).abs) <= 1 )
+
+    infix operator fun plus(other:Position) = Position(x + other.x, y + other.y)
+
+    fun directionOfHead(head: Position):Direction = when(true) {
+        (y == head.y && x <  head.x) -> Direction.Right
+        (y == head.y && x >  head.x) -> Direction.Left
+        (y >  head.y && x == head.x) -> Direction.Up
+        (y <  head.y && x == head.x) -> Direction.Down
+        (y >  head.y && x <  head.x)  -> Direction.UpRight
+        (y >  head.y && x >  head.x)  -> Direction.UpLeft
+        (y <  head.y && x <  head.x)  -> Direction.DownRight
+        (y <  head.y && x >  head.x)  -> Direction.DownLeft
+        else -> Direction.None
+    }
+
+    fun moveTowards(head:Position) = if (isTouching(head)) this else this + directionOfHead(head).offset
+
+    fun moveHead(instruction: Instruction, tails:List<Position>, auditTrail:MutableSet<Position>):Pair<Position, List<Position>> {
+        if (instruction.qty == 0) return Pair(this, tails)
+        val movedHead = this + instruction.direction.offset
+        val newTails = tails.moveTowards(movedHead)
+        auditTrail.add(newTails.last())
+        return movedHead.moveHead(Instruction(instruction.direction, instruction.qty - 1), newTails, auditTrail )
+    }
+}
+
+fun List<Position>.moveTowards(head:Position) =
+    mapIndexed { index, tail -> if (index == 0) tail.moveTowards(head) else tail.moveTowards(get(index - 1))}
+
+
+fun List<Instruction>.processAll(start:Position, tailSize:Int, auditTrail:MutableSet<Position>):Pair<Position,List<Position>> {
+    val headAndTail = Pair(start, (1..tailSize).map{start})
+    auditTrail.add(start)
+    return fold(headAndTail){result, instruction ->
+        val (head, tail) = result
+        head.moveHead(instruction, tail, auditTrail)
+    }
+}
+
+fun partOne(data: List<String>):Set<Position> {
+    val start = Position(0,4)
+    val instructions = data.parse()
+    val audit = mutableSetOf(start)
+    val (_, _) = instructions.processAll(start,1, audit)
+    return audit
+}
+
+fun partTwo(data: List<String>):Set<Position> {
+    val start = Position(0,4)
+    val instructions = data.parse()
+    val audit = mutableSetOf(start)
+    val (_, _) = instructions.processAll(start,9, audit)
+    return audit
+}
+
+
+
+val Int.abs get() = if (this < 0) -this else this
--- day09/src/test/kotlin/MainTest.kt
+++ day09/src/test/kotlin/MainTest.kt
@@ -2,14 +2,100 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "Position[0,0] isTouching Position[-1,-1]" {
+            Position(0,0) isTouching Position(-1,-1) shouldBe true
+        }
+        "Position[0,0] is not Touching Position[-1,-2]" {
+            Position(0,0) isTouching Position(-1,-2) shouldBe false
+        }
+        "when Tail[1,1] and Head[3,1] tail should move to [2,1]" {
+            val head = Position(3,1)
+            val tail = Position(1,1)
+            tail.moveTowards(head) shouldBe Position(2,1)
+        }
+        "when Tail[1,1] and Head[1,3] tail should move to [1,2]" {
+            val head = Position(1,3)
+            val tail = Position(1,1)
+            tail.moveTowards(head) shouldBe Position(1,2)
+        }
+        "when Tail[1,3] and Head[2,1] tail should move to [2,2]" {
+            val head = Position(2,1)
+            val tail = Position(1,3)
+            tail.moveTowards(head) shouldBe Position(2,2)
+        }
+        "when Tail[1,3] and Head[3,2] tail should move to [2,2]" {
+            val head = Position(3,2)
+            val tail = Position(1,3)
+            tail.moveTowards(head) shouldBe Position(2,2)
+        }
+        "parse instructions correctly" {
+            val instructions = sampleInput.parse()
+            instructions.size shouldBe 8
+            instructions[0] shouldBe Instruction(Direction.Right, 4)
+            instructions[1] shouldBe Instruction(Direction.Up, 4)
+            instructions[2] shouldBe Instruction(Direction.Left, 3)
+            instructions[3] shouldBe Instruction(Direction.Down, 1)
+        }
+        "move head and tail at [0,4] Right 4 to [4,4] " {
+            val head = Position(0,4)
+            val tail = listOf(Position(0,4))
+            val instruction = Instruction(Direction.Right,4)
+            val audit = mutableSetOf(tail.last())
+            val (newHead, newTail) = head.moveHead(instruction,tail,audit)
+            newHead shouldBe Position(4,4)
+            newTail shouldBe listOf(Position(3,4))
+            audit shouldBe setOf(Position(0,4),Position(1,4),Position(2,4),Position(3,4))
+        }
+        "following all instructions causes tail to visit 13 places" {
+            val start = Position(0,4)
+            val instructions = sampleInput.parse()
+            val audit = mutableSetOf(start)
+            val (head, tail) = instructions.processAll(start, 1, audit)
+            head shouldBe Position(2,2)
+            tail shouldBe listOf(Position(1,2))
+            audit.size shouldBe 13
+        }
+        "partOne with sample input is 13" {
+            partOne(sampleInput).size shouldBe 13
+        }
+        "partOne with puzzle input is 5619" {
+            partOne(puzzleInput).size shouldBe 5619
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "following all instructions causes tail to visit 1 place" {
+            val start = Position(0,4)
+            val instructions = sampleInput.parse()
+            val audit = mutableSetOf(start)
+            val (head, tails) = instructions.processAll(start, 9, audit)
+            head shouldBe Position(2,2)
+            tails[0] shouldBe Position(1,2)
+            tails[1] shouldBe Position(2,2)
+            tails[2] shouldBe Position(3,2)
+            tails[3] shouldBe Position(2,2)
+            tails[4] shouldBe Position(1,3)
+            tails[5] shouldBe Position(0,4)
+            tails[6] shouldBe Position(0,4)
+            tails[7] shouldBe Position(0,4)
+            tails[8] shouldBe Position(0,4)
+            audit.size shouldBe 1
+        }
+        "partTwo with sample input is 1" {
+            partTwo(sampleInput).size shouldBe 1
+        }
+        "partTwo with puzzle input is 2376" {
+            partTwo(puzzleInput).size shouldBe 2376
         }
     })
 })
