b49d3277091741de1455cd9bec0d8305ceb7ecbd
Mike Neilens <mike.neilens@gmail.com>
2022-12-09 19:06:44 +0000

    day09 refactor

--- day09/src/main/kotlin/Main.kt
+++ day09/src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
-enum class Direction (val offset:Position, val isDirection:(Position, Position)->Boolean) {
-    Right(offset = Position(1,0), isDirection = {t, h -> (t.y == h.y && t.x < h.x)}),
-    Left(offset = Position(-1,0), isDirection = {t, h -> (t.y == h.y && t.x > h.x)}),
-    Up(offset = Position(0,-1), isDirection = {t, h -> (t.x == h.x && t.y > h.y)}),
-    Down(offset = Position(0,1),isDirection = {t, h -> (t.x == h.x && t.y < h.y)}),
-    UpRight(offset = Position(1,-1), isDirection = {t, h -> (t.y > h.y && t.x < h.x)}),
-    UpLeft(offset = Position(-1,-1), isDirection = {t, h -> (t.y > h.y && t.x > h.x)}),
-    DownRight(offset = Position(1,1), isDirection = {t, h -> (t.y < h.y && t.x < h.x)}),
-    DownLeft(offset = Position(-1,1), isDirection = {t, h -> (t.y < h.y && t.x > h.x)}),
-    None(offset = Position(0,0), isDirection = {t, h -> t == h})
+enum class Direction (val offset:Position, val isDirectionWhen:(Position, Position)->Boolean) {
+    None(offset = Position(0,0), isDirectionWhen = { t, h -> t == h}),
+    Right(offset = Position(1,0), isDirectionWhen = { t, h -> (t.y == h.y && t.x < h.x)}),
+    Left(offset = Position(-1,0), isDirectionWhen = { t, h -> (t.y == h.y && t.x > h.x)}),
+    Up(offset = Position(0,-1), isDirectionWhen = { t, h -> (t.x == h.x && t.y > h.y)}),
+    Down(offset = Position(0,1),isDirectionWhen = { t, h -> (t.x == h.x && t.y < h.y)}),
+    UpRight(offset = Position(1,-1), isDirectionWhen = { t, h -> (t.y > h.y && t.x < h.x)}),
+    UpLeft(offset = Position(-1,-1), isDirectionWhen = { t, h -> (t.y > h.y && t.x > h.x)}),
+    DownRight(offset = Position(1,1), isDirectionWhen = { t, h -> (t.y < h.y && t.x < h.x)}),
+    DownLeft(offset = Position(-1,1), isDirectionWhen = { t, h -> (t.y < h.y && t.x > h.x)}),
 }
 
 data class Instruction(val direction:Direction, val qty:Int)
@@ -25,7 +25,7 @@ data class Position(val x:Int, val y:Int) {
 
     infix operator fun plus(other:Position) = Position(x + other.x, y + other.y)
 
-    fun directionOfHead(head: Position) = Direction.values().first { it.isDirection(this, head)}
+    private fun directionOfHead(head: Position) = Direction.values().first { it.isDirectionWhen(this, head)}
 
     fun moveTowards(head:Position) = if (isTouching(head)) this else this + directionOfHead(head).offset
 
--- day09/src/test/kotlin/MainTest.kt
+++ day09/src/test/kotlin/MainTest.kt
@@ -19,6 +19,51 @@ class MainTest: WordSpec( {
         "Position[0,0] is not Touching Position[-1,-2]" {
             Position(0,0) isTouching Position(-1,-2) shouldBe false
         }
+        "Direction to move should be Right when tail is [1,1] and head is [3,1]" {
+            val tail_1_1 = Position(1,1)
+            val head_3_1 = Position(3,1)
+            Direction.Right.isDirectionWhen(tail_1_1, head_3_1 ) shouldBe true
+        }
+        "Direction to move should be Left when tail is [3,1] and head is [1,1]" {
+            val tail_3_1 = Position(3,1)
+            val head_1_1 = Position(1,1)
+            Direction.Left.isDirectionWhen(tail_3_1, head_1_1 ) shouldBe true
+        }
+        "Direction to move should be Up when tail is [1,3] and head is [1,1]" {
+            val tail_1_3 = Position(1,3)
+            val head_1_1 = Position(1,1)
+            Direction.Up.isDirectionWhen(tail_1_3, head_1_1 ) shouldBe true
+        }
+        "Direction to move should be Down when tail is [1,1] and head is [1,3]" {
+            val tail_1_1 = Position(1,1)
+            val head_1_3 = Position(1,3)
+            Direction.Down.isDirectionWhen(tail_1_1, head_1_3 ) shouldBe true
+        }
+        "Direction to move should be None when tail is [3,3] and head is [3,3]" {
+            val tail_3_3 = Position(3,3)
+            val head_3_3 = Position(3,3)
+            Direction.None.isDirectionWhen(tail_3_3, head_3_3 ) shouldBe true
+        }
+        "Direction to move should be DownRight when tail is [1,1] and head is [3,2]" {
+            val tail_1_1 = Position(1,1)
+            val head_3_2 = Position(3,2)
+            Direction.DownRight.isDirectionWhen(tail_1_1, head_3_2 ) shouldBe true
+        }
+        "Direction to move should be DownLeft when tail is [1,1] and head is [0,3]" {
+            val tail_1_1 = Position(1,1)
+            val head_0_3 = Position(0,3)
+            Direction.DownLeft.isDirectionWhen(tail_1_1, head_0_3 ) shouldBe true
+        }
+        "Direction to move should be UpRight when tail is [2,2] and head is [3,0]" {
+            val tail_2_2 = Position(2,2)
+            val head_3_0 = Position(3,0)
+            Direction.UpRight.isDirectionWhen(tail_2_2, head_3_0 ) shouldBe true
+        }
+        "Direction to move should be UpLeft when tail is [2,2] and head is [1,0]" {
+            val tail_2_2 = Position(2,2)
+            val head_1_0 = Position(1,0)
+            Direction.UpLeft.isDirectionWhen(tail_2_2, head_1_0 ) shouldBe true
+        }
         "when Tail[1,1] and Head[3,1] tail should move to [2,1]" {
             val head = Position(3,1)
             val tail = Position(1,1)
