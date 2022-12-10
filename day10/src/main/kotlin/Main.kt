3cfe654a92a72cceb2ab292cbecfaf8736ca1a0d
Mike Neilens <mike.neilens@gmail.com>
2022-12-10 11:29:49 +0000

    day10 needs lots of tidying

--- day10/src/main/kotlin/Main.kt
+++ day10/src/main/kotlin/Main.kt
@@ -1,1 +1,60 @@
-fun helloWorld() = "hello world"
+sealed class ProgramStep {
+    object NOOP:ProgramStep()
+    object AddX:ProgramStep(){val process = {x:Int, y:Int -> x + y}}
+    data class Number(val value:Int):ProgramStep() {val instruction = AddX}
+}
+
+data class Program(val programSteps:List<ProgramStep>) {
+    fun run():List<Pair<Int, Int>> {
+        var x = 1
+        return programSteps.map{programStep -> if (programStep is ProgramStep.Number) {
+            val duringAndAfter = calcDuringAndAfter(programStep, x)
+            x = duringAndAfter.second
+            duringAndAfter
+        } else Pair(x,x) }
+    }
+}
+
+fun calcDuringAndAfter(programStep:ProgramStep.Number, x:Int):Pair<Int, Int> = Pair(x, programStep.instruction.process(x, programStep.value) )
+
+fun List<Pair<Int,Int>>.signalStringDuring(cycle:Int) = get(cycle -1 ).first * cycle
+
+fun List<String>.parseToProgram():Program {
+    val programSteps  = mutableListOf<ProgramStep>()
+    forEach { line ->
+        when (line.split(" ")[0]) {
+            "addx" -> {
+                programSteps.add(ProgramStep.AddX )
+                programSteps.add(ProgramStep.Number(line.split(" ")[1].toInt()))
+            }
+            else -> programSteps.add(ProgramStep.NOOP)
+        }
+    }
+    return Program(programSteps)
+}
+
+fun partOne(data:List<String>): Int {
+    val program = data.parseToProgram()
+    val output = program.run()
+    return output.signalStringDuring(20) + output.signalStringDuring(60) +
+            output.signalStringDuring(100) + output.signalStringDuring(140) +
+            output.signalStringDuring(180) + output.signalStringDuring(220)
+}
+
+fun createImage(result:List<Pair<Int, Int>>):List<String> {
+    val results:List<List<Pair<Int, Int>>> = result.chunked(40)
+    return results.map{crtLine(it)}
+}
+
+fun crtLine(result:List<Pair<Int, Int>>):String =
+    (0..39).map{ pixelPosition ->
+        if (result[pixelPosition].first == pixelPosition || result[pixelPosition].first - 1 == pixelPosition || result[pixelPosition].first + 1 == pixelPosition      )
+            '#'
+        else '.'  }.joinToString("")
+
+fun partTwo(data: List<String>):List<String> {
+    val results = data.parseToProgram().run()
+    val image = createImage(results)
+    image.forEach { println(it) }
+    return image
+}
--- day10/src/test/kotlin/MainTest.kt
+++ day10/src/test/kotlin/MainTest.kt
@@ -2,14 +2,92 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+
+    val sampleInput = """
+        noop
+        addx 3
+        addx -5
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse sample input into a program" {
+            val program = sampleInput.parseToProgram()
+            program.programSteps[0] shouldBe ProgramStep.NOOP
+            program.programSteps[1] shouldBe ProgramStep.AddX
+            program.programSteps[2] shouldBe ProgramStep.Number(3)
+            program.programSteps[3] shouldBe ProgramStep.AddX
+            program.programSteps[4] shouldBe ProgramStep.Number(-5)
+        }
+        "running program from sample input gves x values of  1,1,4,4,-1 " {
+            val program = sampleInput.parseToProgram()
+            program.run() shouldBe listOf(Pair(1, 1), Pair(1, 1), Pair(1, 4), Pair(4, 4), Pair(4, -1))
+        }
+        "running program with large sample gives signal strength of 420 at cycle 20" {
+            val program = largeSample.parseToProgram()
+            val result = program.run()
+            result.signalStringDuring(cycle = 20) shouldBe 420
+        }
+        "running program with large sample gives signal strength of 1140 at cycle 60" {
+            val program = largeSample.parseToProgram()
+            val result = program.run()
+            result.signalStringDuring(cycle = 60) shouldBe 1140
+        }
+        "running program with large sample gives signal strength of 1800 at cycle 100" {
+            val program = largeSample.parseToProgram()
+            val result = program.run()
+            result.signalStringDuring(cycle = 100) shouldBe 1800
+        }
+        "running program with large sample gives signal strength of 2880 at cycle 180" {
+            val program = largeSample.parseToProgram()
+            val result = program.run()
+            result.signalStringDuring(cycle = 180) shouldBe 2880
+        }
+        "running program with large sample gives signal strength of 3960 at cycle 220" {
+            val program = largeSample.parseToProgram()
+            val result = program.run()
+            result.signalStringDuring(cycle = 220) shouldBe 3960
+        }
+        "part one with large sample gives total signal strength of 13140" {
+            partOne(largeSample) shouldBe 13140
+        }
+        "part one with puzzle input gives total signal strength of 13920" {
+            partOne(puzzleInput) shouldBe 13920
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "draw first line of CRT using large sample data" {
+            val program = largeSample.parseToProgram()
+            val result = program.run().take(40)
+            val crtLine = crtLine(result)
+            crtLine shouldBe "##..##..##..##..##..##..##..##..##..##.."
+        }
+        "draw 2nd line of CRT using large sample data" {
+            val program = largeSample.parseToProgram()
+            val result = program.run().drop(40).take(40)
+            val crtLine = crtLine(result)
+            crtLine shouldBe "###...###...###...###...###...###...###."
+        }
+        "draw crt image using large sample data" {
+            val expectedResult = """
+                ##..##..##..##..##..##..##..##..##..##..
+                ###...###...###...###...###...###...###.
+                ####....####....####....####....####....
+                #####.....#####.....#####.....#####.....
+                ######......######......######......####
+                #######.......#######.......#######.....
+            """.trimIndent().split("\n")
+            val program = largeSample.parseToProgram()
+            createImage(program.run()) shouldBe expectedResult
+        }
+        "part two using puzzle input" {
+            val expectedResult = """
+                ####..##..#....#..#.###..#....####...##.
+                #....#..#.#....#..#.#..#.#....#.......#.
+                ###..#....#....####.###..#....###.....#.
+                #....#.##.#....#..#.#..#.#....#.......#.
+                #....#..#.#....#..#.#..#.#....#....#..#.
+                ####..###.####.#..#.###..####.#.....##..
+            """.trimIndent().split("\n")
+            partTwo(puzzleInput) shouldBe expectedResult
         }
     })
 })
