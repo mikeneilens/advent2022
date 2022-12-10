c52a1619f36cd1a1627819e4e83c4303901a936e
Mike Neilens <mike.neilens@gmail.com>
2022-12-10 16:48:49 +0000

    day09 refactor to use linked list

--- day09/src/main/kotlin/Main.kt
+++ day09/src/main/kotlin/Main.kt
@@ -1,63 +1,58 @@
-enum class Direction (val offset:Position, val isDirectionWhen:(Position, Position)->Boolean) {
-    None(offset = Position(0,0), isDirectionWhen = { t, h -> t == h}),
-    Touching(offset = Position(0,0), isDirectionWhen = { t, h -> ((t.x - h.x).abs) <= 1 && ((t.y - h.y).abs) <= 1 }),
-    Right(offset = Position(1,0), isDirectionWhen = { t, h -> (t.y == h.y && t.x < h.x)}),
-    Left(offset = Position(-1,0), isDirectionWhen = { t, h -> (t.y == h.y && t.x > h.x)}),
-    Up(offset = Position(0,-1), isDirectionWhen = { t, h -> (t.x == h.x && t.y > h.y)}),
-    Down(offset = Position(0,1),isDirectionWhen = { t, h -> (t.x == h.x && t.y < h.y)}),
-    UpRight(offset = Position(1,-1), isDirectionWhen = { t, h -> (t.y > h.y && t.x < h.x)}),
-    UpLeft(offset = Position(-1,-1), isDirectionWhen = { t, h -> (t.y > h.y && t.x > h.x)}),
-    DownRight(offset = Position(1,1), isDirectionWhen = { t, h -> (t.y < h.y && t.x < h.x)}),
-    DownLeft(offset = Position(-1,1), isDirectionWhen = { t, h -> (t.y < h.y && t.x > h.x)}),
+import java.lang.Math.abs
+
+enum class Direction (val offset:Position, val isDirectionFor:(Position, Position)->Boolean) {
+    None(offset = Position(0,0), isDirectionFor = { t, h -> t == h}),
+    Touching(offset = Position(0,0), isDirectionFor = { t, h -> ( abs(t.x - h.x)) <= 1 && (abs(t.y - h.y)) <= 1 }),
+    Right(offset = Position(1,0), isDirectionFor = { t, h -> (t.y == h.y && t.x < h.x)}),
+    Left(offset = Position(-1,0), isDirectionFor = { t, h -> (t.y == h.y && t.x > h.x)}),
+    Up(offset = Position(0,-1), isDirectionFor = { t, h -> (t.x == h.x && t.y > h.y)}),
+    Down(offset = Position(0,1),isDirectionFor = { t, h -> (t.x == h.x && t.y < h.y)}),
+    UpRight(offset = Position(1,-1), isDirectionFor = { t, h -> (t.y > h.y && t.x < h.x)}),
+    UpLeft(offset = Position(-1,-1), isDirectionFor = { t, h -> (t.y > h.y && t.x > h.x)}),
+    DownRight(offset = Position(1,1), isDirectionFor = { t, h -> (t.y < h.y && t.x < h.x)}),
+    DownLeft(offset = Position(-1,1), isDirectionFor = { t, h -> (t.y < h.y && t.x > h.x)}),
 }
 
 data class Instruction(val direction:Direction, val qty:Int)
 
-fun List<String>.parse() = map(String::toInstruction)
+fun List<String>.parseToInstructions() = map(String::toInstruction)
 
-fun String.toInstruction():Instruction {
-    val direction = Direction.values().first{it.name.startsWith(split(" ")[0])}
-    val qty = split(" ")[1].toInt()
-    return Instruction(direction,qty)
-}
+fun String.toInstruction():Instruction = Instruction(toDirection(), toQty())
+
+fun String.toDirection() = Direction.values().first{it.name.startsWith(split(" ")[0])}
+fun String.toQty() = split(" ")[1].toInt()
 
 data class Position(val x:Int, val y:Int) {
-
     infix operator fun plus(other:Position) = Position(x + other.x, y + other.y)
+}
 
-    private fun directionOfHead(head: Position) = Direction.values().first { it.isDirectionWhen(this, head)}
+data class Knot(var pos:Position, var next:Knot? = null) {
 
-    fun moveTowards(head:Position) =  this + directionOfHead(head).offset
-
-    fun moveHead(instruction: Instruction, tails:List<Position>, auditTrail:MutableSet<Position>):Pair<Position, List<Position>> {
-        if (instruction.qty == 0) return Pair(this, tails)
-        val movedHead = this + instruction.direction.offset
-        val newTails = tails.moveTowards(movedHead)
-        auditTrail.add(newTails.last())
-        return movedHead.moveHead(Instruction(instruction.direction, instruction.qty - 1), newTails, auditTrail )
+    fun move(direction:Direction, audit:MutableSet<Position>) {
+        pos = pos + direction.offset
+        next?.let { next ->
+            val nextDirection = Direction.values().first { direction -> direction.isDirectionFor(next.pos, pos)}
+            next.move(nextDirection, audit)
+        } ?: audit.add(pos)
     }
 }
 
-fun List<Position>.moveTowards(head:Position) =
-    mapIndexed { index, tail -> if (index == 0) tail.moveTowards(head) else tail.moveTowards(get(index - 1))}
-
-
-fun List<Instruction>.processAll(start:Position, tails:List<Position>, auditTrail:MutableSet<Position>):Pair<Position,List<Position>> =
-    fold(Pair(start, tails)){result, instruction ->
-        val (head, tail) = result
-        head.moveHead(instruction, tail, auditTrail)
+fun process(knot:Knot, direction:Direction, qty:Int, audit:MutableSet<Position>) {
+    if (qty > 0)  {
+        knot.move(direction, audit)
+        process(knot, direction,qty -1, audit)
     }
+}
 
-
-fun partOne(data: List<String>, tailSize:Int = 1):Set<Position> {
-    val head = Position(0,4)
-    val tails = (1..tailSize).map{head}
-    val instructions = data.parse()
+fun partOne(data:List<String>, knot:Knot = Knot(Position(0,0), Knot(Position(0,0),null)) ):Set<Position> {
+    val instructions = data.parseToInstructions()
     val audit = mutableSetOf<Position>()
-    val (_, _) = instructions.processAll(head, tails,audit)
+    instructions.forEach { process(knot, it.direction, it.qty, audit)}
     return audit
 }
 
-fun partTwo(data: List<String>):Set<Position> = partOne(data, tailSize = 9)
-
-val Int.abs get() = if (this < 0) -this else this
+fun partTwo(data:List<String>):Set<Position> {
+    val knots = (0..9).map{Knot(Position(0,0))}
+    knots.windowed(2,1).forEach { (a, b) -> a.next = b }
+    return partOne(data, knots.first())
+}
--- day09/src/test/kotlin/MainTest.kt
+++ day09/src/test/kotlin/MainTest.kt
@@ -14,130 +14,74 @@ class MainTest: WordSpec( {
     """.trimIndent().split("\n")
     "Part one" should ({
         "Position[0,0] isTouching Position[-1,-1]" {
-            Direction.Touching.isDirectionWhen(Position(-1,-1),Position(0,0)) shouldBe true
+            Direction.Touching.isDirectionFor(Position(-1,-1),Position(0,0)) shouldBe true
         }
         "Position[0,0] is not Touching Position[-1,-2]" {
-            Direction.Touching.isDirectionWhen(Position(0,0),Position(-1,-2)) shouldBe false
+            Direction.Touching.isDirectionFor(Position(0,0),Position(-1,-2)) shouldBe false
         }
         "Direction to move should be Right when tail is [1,1] and head is [3,1]" {
             val tail_1_1 = Position(1,1)
             val head_3_1 = Position(3,1)
-            Direction.Right.isDirectionWhen(tail_1_1, head_3_1 ) shouldBe true
+            Direction.Right.isDirectionFor(tail_1_1, head_3_1 ) shouldBe true
         }
         "Direction to move should be Left when tail is [3,1] and head is [1,1]" {
             val tail_3_1 = Position(3,1)
             val head_1_1 = Position(1,1)
-            Direction.Left.isDirectionWhen(tail_3_1, head_1_1 ) shouldBe true
+            Direction.Left.isDirectionFor(tail_3_1, head_1_1 ) shouldBe true
         }
         "Direction to move should be Up when tail is [1,3] and head is [1,1]" {
             val tail_1_3 = Position(1,3)
             val head_1_1 = Position(1,1)
-            Direction.Up.isDirectionWhen(tail_1_3, head_1_1 ) shouldBe true
+            Direction.Up.isDirectionFor(tail_1_3, head_1_1 ) shouldBe true
         }
         "Direction to move should be Down when tail is [1,1] and head is [1,3]" {
             val tail_1_1 = Position(1,1)
             val head_1_3 = Position(1,3)
-            Direction.Down.isDirectionWhen(tail_1_1, head_1_3 ) shouldBe true
+            Direction.Down.isDirectionFor(tail_1_1, head_1_3 ) shouldBe true
         }
         "Direction to move should be None when tail is [3,3] and head is [3,3]" {
             val tail_3_3 = Position(3,3)
             val head_3_3 = Position(3,3)
-            Direction.None.isDirectionWhen(tail_3_3, head_3_3 ) shouldBe true
+            Direction.None.isDirectionFor(tail_3_3, head_3_3 ) shouldBe true
         }
         "Direction to move should be DownRight when tail is [1,1] and head is [3,2]" {
             val tail_1_1 = Position(1,1)
             val head_3_2 = Position(3,2)
-            Direction.DownRight.isDirectionWhen(tail_1_1, head_3_2 ) shouldBe true
+            Direction.DownRight.isDirectionFor(tail_1_1, head_3_2 ) shouldBe true
         }
         "Direction to move should be DownLeft when tail is [1,1] and head is [0,3]" {
             val tail_1_1 = Position(1,1)
             val head_0_3 = Position(0,3)
-            Direction.DownLeft.isDirectionWhen(tail_1_1, head_0_3 ) shouldBe true
+            Direction.DownLeft.isDirectionFor(tail_1_1, head_0_3 ) shouldBe true
         }
         "Direction to move should be UpRight when tail is [2,2] and head is [3,0]" {
             val tail_2_2 = Position(2,2)
             val head_3_0 = Position(3,0)
-            Direction.UpRight.isDirectionWhen(tail_2_2, head_3_0 ) shouldBe true
+            Direction.UpRight.isDirectionFor(tail_2_2, head_3_0 ) shouldBe true
         }
         "Direction to move should be UpLeft when tail is [2,2] and head is [1,0]" {
             val tail_2_2 = Position(2,2)
             val head_1_0 = Position(1,0)
-            Direction.UpLeft.isDirectionWhen(tail_2_2, head_1_0 ) shouldBe true
+            Direction.UpLeft.isDirectionFor(tail_2_2, head_1_0 ) shouldBe true
         }
-        "when Tail[1,1] and Head[3,1] tail should move to [2,1]" {
-            val head = Position(3,1)
-            val tail = Position(1,1)
-            tail.moveTowards(head) shouldBe Position(2,1)
-        }
-        "when Tail[1,1] and Head[1,3] tail should move to [1,2]" {
-            val head = Position(1,3)
-            val tail = Position(1,1)
-            tail.moveTowards(head) shouldBe Position(1,2)
-        }
-        "when Tail[1,3] and Head[2,1] tail should move to [2,2]" {
-            val head = Position(2,1)
-            val tail = Position(1,3)
-            tail.moveTowards(head) shouldBe Position(2,2)
-        }
-        "when Tail[1,3] and Head[3,2] tail should move to [2,2]" {
-            val head = Position(3,2)
-            val tail = Position(1,3)
-            tail.moveTowards(head) shouldBe Position(2,2)
-        }
+
         "parse instructions correctly" {
-            val instructions = sampleInput.parse()
+            val instructions = sampleInput.parseToInstructions()
             instructions.size shouldBe 8
             instructions[0] shouldBe Instruction(Direction.Right, 4)
             instructions[1] shouldBe Instruction(Direction.Up, 4)
             instructions[2] shouldBe Instruction(Direction.Left, 3)
             instructions[3] shouldBe Instruction(Direction.Down, 1)
         }
-        "move head and tail at [0,4] Right 4 to [4,4] " {
-            val head = Position(0,4)
-            val tail = listOf(Position(0,4))
-            val instruction = Instruction(Direction.Right,4)
-            val audit = mutableSetOf(tail.last())
-            val (newHead, newTail) = head.moveHead(instruction,tail,audit)
-            newHead shouldBe Position(4,4)
-            newTail shouldBe listOf(Position(3,4))
-            audit shouldBe setOf(Position(0,4),Position(1,4),Position(2,4),Position(3,4))
-        }
-        "following all instructions causes tail to visit 13 places" {
-            val start = Position(0,4)
-            val tails = (1..1).map{start}
-            val instructions = sampleInput.parse()
-            val audit = mutableSetOf(start)
-            val (head, tail) = instructions.processAll(start, tails, audit)
-            head shouldBe Position(2,2)
-            tail shouldBe listOf(Position(1,2))
-            audit.size shouldBe 13
-        }
-        "partOne with sample input is 13" {
+        "partOn with sample input is 13" {
             partOne(sampleInput).size shouldBe 13
         }
         "partOne with puzzle input is 5619" {
             partOne(puzzleInput).size shouldBe 5619
         }
     })
+
     "Part two" should ({
-        "following all instructions causes tail to visit 1 place" {
-            val start = Position(0,4)
-            val startTails = (1..9).map{start}
-            val instructions = sampleInput.parse()
-            val audit = mutableSetOf(start)
-            val (head, tails) = instructions.processAll(start, startTails, audit)
-            head shouldBe Position(2,2)
-            tails[0] shouldBe Position(1,2)
-            tails[1] shouldBe Position(2,2)
-            tails[2] shouldBe Position(3,2)
-            tails[3] shouldBe Position(2,2)
-            tails[4] shouldBe Position(1,3)
-            tails[5] shouldBe Position(0,4)
-            tails[6] shouldBe Position(0,4)
-            tails[7] shouldBe Position(0,4)
-            tails[8] shouldBe Position(0,4)
-            audit.size shouldBe 1
-        }
         "partTwo with sample input is 1" {
             partTwo(sampleInput).size shouldBe 1
         }
