d39fa407f63c51606b934407a2ea0afb493e4f37
Mike Neilens <mike.neilens@gmail.com>
2022-12-11 13:30:18 +0000

    day11

--- day11/src/main/kotlin/Main.kt
+++ day11/src/main/kotlin/Main.kt
@@ -1,1 +1,70 @@
-fun helloWorld() = "hello world"
+data class Monkey(
+    val monkeyNumber:Int,
+    val items:MutableList<Long>,
+    val operation:(Long)->Long,
+    val monkeyToThrowTo:(Long)->Int,
+    var worryReducer:(Long)->Long = { n -> n /3},
+    var noOfOperations:Int = 0)
+
+fun List<String>.toMonkey(reduceWorry: (Long)->Long)
+= Monkey(monkeyNumber(),startingItems(), operation(reduceWorry), monkeyToThrowTo(testDivisible()),reduceWorry)
+
+fun List<String>.monkeyNumber() = first().removePrefix("Monkey ").removeSuffix(":").toInt()
+
+fun List<String>.startingItems() = get(1).removePrefix("  Starting items: ").split(", ").map(String::toLong).toMutableList()
+
+fun List<String>.operation(reduceWorry: (Long)->Long): (Long)-> Long {
+    val (symbol, param2) = get(2).removePrefix("  Operation: new = ").split(" ").drop(1)
+    return operation(symbol, param2, reduceWorry)
+}
+fun operation(symbol:String, param2:String, reduceWorry: (Long)->Long): (Long)->Long {
+    return if (symbol == "+") {old:Long -> reduceWorry(old + param2.toLong()) }
+    else if (symbol == "*" && param2 == "old") {old:Long -> reduceWorry( old * old) }
+    else  {old:Long -> reduceWorry(old * param2.toInt()) }
+}
+
+fun List<String>.testDivisibleBy(): Long = get(3).removePrefix("  Test: divisible by ").toLong()
+
+fun List<String>.testDivisible(): (Long, Int, Int)-> Int {
+    return {n:Long, trueMonkey:Int, falseMonkey:Int -> if ((n % testDivisibleBy()) == 0L) trueMonkey else falseMonkey}
+}
+
+fun List<String>.monkeyToThrowTo(divisibleRule:(Long, Int, Int)-> Int): (Long) -> Int {
+    val trueMonkey = get(4).removePrefix("    If true: throw to monkey ").toInt()
+    val falseMonkey = get(5).removePrefix("    If false: throw to monkey ").toInt()
+    return { n:Long -> divisibleRule(n, trueMonkey, falseMonkey)}
+}
+
+fun List<List<String>>.toMonkeys(reduceWorry: (Long)->Long) = map{it.toMonkey(reduceWorry)}
+
+fun List<Monkey>.processRound() = forEach{monkey -> processAll(monkey)}
+
+fun List<Monkey>.processAll(monkey: Monkey) {
+    monkey.items.indices.forEach { process(monkey, it) }
+    monkey.items.removeAll{true}
+}
+
+fun List<Monkey>.process(monkey:Monkey, itemNdx:Int) {
+    val item = monkey.items[itemNdx]
+    val worryLevel = monkey.operation(item)
+    val monkeyToThrowTo = monkey.monkeyToThrowTo( worryLevel)
+    get(monkeyToThrowTo).items.add(worryLevel)
+    monkey.noOfOperations += 1
+}
+
+fun partOne(data: List<List<String>>):Int {
+    val monkeys = data.toMonkeys(reduceWorry =  {n -> n / 3})
+    repeat(20){monkeys.processRound()}
+    val (m1, m2) = monkeys.sortedBy { it.noOfOperations }.takeLast(2)
+    return m1.noOfOperations * m2.noOfOperations
+}
+
+fun partTwo(data: List<List<String>>):Long {
+    val lowestCommonMultiplier = data.map{it.testDivisibleBy()}.toLowestCommonMultiplier()
+    val monkeys = data.toMonkeys{n -> n % lowestCommonMultiplier}
+    repeat(10000){monkeys.processRound()}
+    val (m1, m2) = monkeys.sortedBy { it.noOfOperations }.takeLast(2)
+    return m1.noOfOperations.toLong() * m2.noOfOperations.toLong()
+}
+
+fun List<Long>.toLowestCommonMultiplier() = reduce { lcm, n -> lcm * n }
--- day11/src/test/kotlin/MainTest.kt
+++ day11/src/test/kotlin/MainTest.kt
@@ -2,14 +2,128 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+    """.trimIndent().split("\n\n").map{it.split("\n")}
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "Monkey number of first monkey" {
+            sampleInput[0].monkeyNumber() shouldBe 0
+        }
+        "Starting items of first monkey is 79, 98" {
+            sampleInput[0].startingItems() shouldBe listOf(79,98)
+        }
+        "operation for old * 19 /3" {
+            val operation = operation("*", "19", {n -> n /3})
+            operation(2) shouldBe 12
+        }
+        "operation for old * old /3" {
+            val operation = operation("*", "old", {n -> n /3})
+            operation(5) shouldBe 8        }
+        "operation for (old + 19) /3" {
+            val operation = operation("+", "19", {n -> n /3})
+            operation(5) shouldBe 8
+        }
+        "operation of first monkey is old * 19 /3" {
+            sampleInput[0].operation({n -> n /3})(2) shouldBe 12
+        }
+        "test divisible of first monkey is 23" {
+            val trueMonkey = 1
+            val falseMonkey = 2
+            sampleInput[0].testDivisible()(46, trueMonkey, falseMonkey) shouldBe trueMonkey
+            sampleInput[0].testDivisible()(47, trueMonkey, falseMonkey) shouldBe falseMonkey
+        }
+        "throw to monkey 2 if true and monkey 3 if false" {
+            val divisibleRule = sampleInput[0].testDivisible()
+            sampleInput[0].monkeyToThrowTo(divisibleRule)(46) shouldBe 2
+            sampleInput[0].monkeyToThrowTo(divisibleRule)(47) shouldBe 3
+        }
+        "sample input toMonkeys creates a list of 4 monkeys" {
+            val monkeys = sampleInput.toMonkeys({n -> n /3})
+            monkeys.size shouldBe 4
+            monkeys[0].monkeyNumber shouldBe 0
+            monkeys[3].monkeyNumber shouldBe 3
+        }
+        "process first monkey in the Sample Input" {
+            val monkeys = sampleInput.toMonkeys({n -> n /3})
+            monkeys.processAll(monkeys[0])
+            monkeys[0].items shouldBe listOf()
+            monkeys[0].noOfOperations shouldBe 2
+            monkeys[3].items shouldBe listOf(74, 500, 620)
+        }
+        "processing all monkies for one round in the Sample Input" {
+            val monkeys = sampleInput.toMonkeys({n -> n /3})
+            monkeys.processRound()
+            monkeys[0].items shouldBe listOf(20, 23, 27, 26)
+            monkeys[1].items shouldBe listOf(2080, 25, 167, 207, 401, 1046)
+            monkeys[2].items shouldBe listOf()
+            monkeys[3].items shouldBe listOf()
+        }
+        "part one with sample input is 10605" {
+            partOne(sampleInput) shouldBe 10605
+        }
+        "part one with puzzle input is 98280" {
+            partOne(puzzleInput) shouldBe 98280
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "processing all monkies for one round in the Sample Input" {
+            val lowestCommonMultiplier = sampleInput.map{it.testDivisibleBy()}.toLowestCommonMultiplier()
+            val monkeys = sampleInput.toMonkeys({ n -> n % lowestCommonMultiplier  })
+            monkeys.processRound()
+            monkeys[0].noOfOperations shouldBe 2
+            monkeys[1].noOfOperations shouldBe 4
+            monkeys[2].noOfOperations shouldBe 3
+            monkeys[3].noOfOperations shouldBe 6
+        }
+        "processing all monkies for 20 rounds in the Sample Input" {
+            val lowestCommonMultiplier = sampleInput.map{it.testDivisibleBy()}.toLowestCommonMultiplier()
+            val monkeys = sampleInput.toMonkeys({ n -> n % lowestCommonMultiplier  })
+            repeat(20){monkeys.processRound()}
+            monkeys[0].noOfOperations shouldBe 99
+            monkeys[1].noOfOperations shouldBe 97
+            monkeys[2].noOfOperations shouldBe 8
+            monkeys[3].noOfOperations shouldBe 103
+        }
+        "processing all monkies for 1000 rounds in the Sample Input" {
+            val lowestCommonMultiplier = sampleInput.map{it.testDivisibleBy()}.toLowestCommonMultiplier()
+            val monkeys = sampleInput.toMonkeys({ n -> n % lowestCommonMultiplier  })
+            repeat(1000){monkeys.processRound()}
+            monkeys[0].noOfOperations shouldBe 5204
+            monkeys[1].noOfOperations shouldBe 4792
+            monkeys[2].noOfOperations shouldBe 199
+            monkeys[3].noOfOperations shouldBe 5192
+        }
+        "part two with sample input is 2713310158" {
+           partTwo(sampleInput) shouldBe 2713310158
+        }
+        "part two with puzzle input is 2713310158" {
+           partTwo(puzzleInput) shouldBe 17673687232L
         }
     })
 })
