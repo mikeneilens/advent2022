aca123af5b4585f2ac315098ebe9eae72050c688
Mike Neilens <mike.neilens@gmail.com>
2022-12-12 19:27:30 +0000

    day12

--- day12/src/main/kotlin/Main.kt
+++ day12/src/main/kotlin/Main.kt
@@ -1,1 +1,47 @@
-fun helloWorld() = "hello world"
+data class Position(val x:Int, val y:Int) {
+    fun height(heightMap:List<String>):Int = when(val char = heightMap[y][x]) {
+        'S' -> 'a'.code
+        'E' -> 'z'.code
+        else -> char.code
+    }
+
+    operator fun plus(other:Position) = Position(x + other.x, y + other.y)
+
+    fun surroundingPositions(heightMap: List<String>) = listOf(Position(1,0), Position(0,1), Position(-1,0), Position(0,-1))
+        .map{it + this}.filter{(it.y in 0 until heightMap.size) && (it.x in 0 until  heightMap[y].length)}
+}
+
+fun Position.atGoal(heightMap: List<String>, goal:Char) = heightMap[y][x] == goal
+
+fun Position.routes(heightMap:List<String>) =
+    surroundingPositions(heightMap).filter{ ( it.height(heightMap) - this.height(heightMap)) < 2}
+
+fun Position.routes2(heightMap:List<String>) =
+    surroundingPositions(heightMap).filter{  ( this.height(heightMap) - it.height(heightMap)) < 2}
+
+fun findJourneySize(heightMap: List<String>, goal:Char, position:Position, size:Int, journeyStatus: JourneyStatus) {
+    if ((journeyStatus.minSizeForPosition[position] ?: Int.MAX_VALUE) <= size || ( journeyStatus.minSize <= size)) return
+
+    journeyStatus.minSizeForPosition[position] = size
+
+    if (position.atGoal(heightMap,goal)) {
+        journeyStatus.minSize = size
+    } else {
+        val routes = if (goal == 'E') position.routes(heightMap) else position.routes2(heightMap)
+        if (routes.isNotEmpty()) routes.forEach{ route -> findJourneySize(heightMap, goal,route, size + 1 , journeyStatus) }
+    }
+}
+
+class JourneyStatus(val minSizeForPosition:MutableMap<Position, Int> = mutableMapOf() , var minSize:Int = Int.MAX_VALUE  )
+
+fun partOne(data:List<String>, start:Char = 'S', end:Char = 'E'):Int {
+    val y = data.indexOfFirst { it.contains(start) }
+    val x = data[y].indexOfFirst { it == start }
+    val journeyStatus = JourneyStatus()
+    findJourneySize(data, end, Position(x,y),0, journeyStatus)
+    return journeyStatus.minSize
+}
+
+fun partTwo(data:List<String>):Int {
+    return partOne(data, 'E', 'a')
+}
--- day12/src/test/kotlin/MainTest.kt
+++ day12/src/test/kotlin/MainTest.kt
@@ -2,14 +2,60 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "height of position containing a or S is 97" {
+            Position(0,0).height(sampleInput) shouldBe 97
+            Position(1,0).height(sampleInput) shouldBe 97
+            Position(0,1).height(sampleInput) shouldBe 97
+        }
+        "height of position containing z or E is 97" {
+            Position(4,2).height(sampleInput) shouldBe 122
+            Position(5,2).height(sampleInput) shouldBe 122
+        }
+        "surrounding positions of 0,0" {
+            Position(0,0).surroundingPositions(sampleInput) shouldBe listOf(Position(x=1, y=0), Position(x=0, y=1))
+        }
+        "surrounding positions of 1,1" {
+            Position(1,1).surroundingPositions(sampleInput) shouldBe listOf(Position(x=2, y=1), Position(x=1, y=2), Position(x=0, y=1), Position(x=1, y=0))
+        }
+        "surrounding positions of 7,4" {
+            Position(7,4).surroundingPositions(sampleInput) shouldBe listOf(Position(x=6, y=4), Position(x=7, y=3))
+        }
+        "valid routes from 0,0" {
+            Position(0,0).routes(sampleInput) shouldBe listOf(Position(x=1, y=0), Position(x=0, y=1))
+        }
+        "valid routes from 3,1" {
+            Position(3,1).routes(sampleInput).sortedBy{ it.height(sampleInput) } shouldBe listOf(Position(x=3, y=2), Position(x=3, y=0), Position(x=2, y=1)).sortedBy{ it.height(sampleInput) }
+        }
+        "at goal at position 5,2" {
+            Position(5,2).atGoal(sampleInput,'E') shouldBe true
+        }
+        "find journey size from positon 0,0" {
+            val journeyStatus = JourneyStatus()
+            val journeys = findJourneySize(sampleInput, 'E', Position(0,0),0, journeyStatus)
+            journeyStatus.minSize shouldBe 31
+        }
+        "part one with sample input" {
+            partOne(sampleInput,'E','S') shouldBe 31
+            partOne(sampleInput,'S','E') shouldBe 31
+        }
+        "part one with puzzle input" {
+            partOne(puzzleInput,'S','E') shouldBe 497
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two with sample input" {
+           partTwo(sampleInput) shouldBe 29
+        }
+        "part two with puzzle input" {
+           partTwo(puzzleInput) shouldBe 492
         }
     })
 })
