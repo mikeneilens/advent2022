f4c305cae043a2d497facdc65320a09263915fe6
Mike Neilens <mike.neilens@gmail.com>
2022-12-13 22:13:26 +0000

    day13

--- day13/src/main/kotlin/Main.kt
+++ day13/src/main/kotlin/Main.kt
@@ -1,1 +1,106 @@
 fun helloWorld() = "hello world"
+
+fun String.toSymbols():List<String> {
+    val symbols = mutableListOf<String>()
+    var prevWasNum = false
+    forEach{char ->
+        if (char == '[' || char == ']') symbols.add(char.toString())
+        if (char in '0'..'9') {
+            if (prevWasNum)
+                symbols[symbols.lastIndex] = symbols[symbols.lastIndex] + char.toString()
+            else  symbols.add(char.toString())
+            prevWasNum = true
+        } else prevWasNum = false
+    }
+    return symbols
+}
+
+sealed class Item {
+    data class Num(val value:Int):Item()
+    class SubList(val items:List<Item>):Item()
+
+    override fun toString():String {
+        return if (this is Num) return "num $value"
+        else if (this is SubList) return "[ ${ items.map{it.toString() }.joinToString()} ]"
+        else ""
+    }
+}
+
+fun Item.SubList.toList():List<Int> {
+    fun Item.SubList.traverse(l:MutableList<Int> = mutableListOf()) {
+        items.forEach { item -> when(item) {
+            is Item.Num -> l.add(item.value)
+            is Item.SubList -> item.traverse(l)
+        }
+        }
+    }
+    val l = mutableListOf<Int>()
+    traverse(l)
+    return l
+}
+
+fun Item.SubList.NumAt(index:Int, n:Int = 0 ):Int? {
+    if (items.isEmpty()) return null
+    val firstItem = items.first()
+    if (n == index && firstItem is Item.Num) return firstItem.value
+
+    else return Item.SubList(this.items.drop(1)).NumAt(index, n + 1)
+}
+
+fun List<String>.parse(output:Item.SubList = Item.SubList(listOf())):Item.SubList {
+    if (isEmpty()) return output
+    val symbol = first()
+    if (symbol == "]") return output
+    val (item, dropQty)  = if ( symbol.toIntOrNull() != null) Pair(Item.Num(symbol.toInt()),1)
+                            else Pair( drop(1).parse(), drop(1).nextIndex())
+    return drop(dropQty).parse(Item.SubList(output.items + item ))
+}
+
+fun List<String>.nextIndex():Int {
+    var open = 1
+    var ndx = 0
+    while (open >0) {
+        if (this[ndx] == "[") open++
+        if (this[ndx] == "]") open--
+        ndx++
+    }
+    return ndx + 1
+}
+
+fun compare(left:Item, right:Item):Boolean? {
+    val left1 = if (left is Item.Num) Item.SubList(listOf(left)) else left as Item.SubList
+    val right1 = if (right is Item.Num ) Item.SubList(listOf(right)) else right as Item.SubList
+
+    if (left1.items.isEmpty() && right1.items.isNotEmpty()) return true
+    if (right1.items.isEmpty() && left1.items.isNotEmpty()) return false
+    if (right1.items.isEmpty() && left1.items.isEmpty()) return null
+
+    val leftFirst = left1.items.first()
+    val rightFirst = right1.items.first()
+    if (leftFirst is Item.Num && rightFirst is Item.Num) {
+        if (leftFirst.value < rightFirst.value) return true
+        if (rightFirst.value < leftFirst.value) return false
+    }
+    if (leftFirst is Item.SubList || rightFirst is Item.SubList) {
+        val compare = compare(leftFirst,rightFirst)
+        if (compare != null) return compare
+    }
+    return compare(Item.SubList(left1.items.drop(1)), Item.SubList(right1.items.drop(1)) )
+}
+
+fun partOne(data:List<String>):Int {
+    var total = 0
+    data.chunked(2).forEachIndexed { index, pair ->
+        val (left, right) = pair
+        val result = compare(left.toSymbols().parse(),right.toSymbols().parse())
+        if (result is Boolean && result == true) total += index + 1
+    }
+    return total
+}
+
+fun partTwo(data:List<String>):Int {
+    val items = (data + "[[2]]" + "[[6]]"  ). map{it.toSymbols().parse()}
+    val sorted = items.sortedWith{ l:Item.SubList, r:Item.SubList -> if (compare(l,r) == true) -1 else 1 }
+    return (sorted.indexOfFirst{it.toString() == "[ [ [ Num(value=2) ] ] ]"} + 1) * (sorted.indexOfFirst{it.toString() == "[ [ [ Num(value=6) ] ] ]"} + 1)
+
+}
--- day13/src/test/kotlin/MainTest.kt
+++ day13/src/test/kotlin/MainTest.kt
@@ -2,14 +2,115 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        [1,1,3,1,1]
+        [1,1,5,1,1]
+
+        [[1],[2,3,4]]
+        [[1],4]
+
+        [9]
+        [[8,7,6]]
+
+        [[4,4],4,4]
+        [[4,4],4,4,4]
+
+        [7,7,7,7]
+        [7,7,7]
+
+        []
+        [3]
+
+        [[[]]]
+        [[]]
+
+        [1,[2,[3,[4,[5,6,7]]]],8,9]
+        [1,[2,[3,[4,[5,6,0]]]],8,9]
+    """.trimIndent().split("\n").filter { it != "" }
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse string [1,2,3,[4,10],11] into ['1','2','3','[','4','10',']','11',']'" {
+            "[1,2,3,[4,10],11]".toSymbols() shouldBe listOf("[","1","2","3","[","4","10","]","11","]")
+        }
+        "parse 1,2,3 into sublistOf(Item.Num(1), Item.Num(2), Item.Num(3)) " {
+            "1,2,3".toSymbols().parse().toString() shouldBe "[ Num(value=1), Num(value=2), Num(value=3) ]"
+        }
+        "parse 1,[2,3] into sublistOf(Item.Num(1), sublistOf(Item.Num(2), Item.Num(3))) " {
+            "1,[2,3]".toSymbols().parse().toString() shouldBe "[ Num(value=1), [ Num(value=2), Num(value=3) ] ]"
+        }
+        "parse 1,[2,3],4 into sublistOf(Item.Num(1), sublistOf(Item.Num(2), Item.Num(3)), Item.Num(4)) " {
+            "1,[2,3],4".toSymbols().parse().toString() shouldBe "[ Num(value=1), [ Num(value=2), Num(value=3) ], Num(value=4) ]"
+        }
+        "parse [1,[2,[3,[4,[5,6,7]]]],8,9] " {
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]".toSymbols().parse().toString() shouldBe
+                    "[ [ Num(value=1), [ Num(value=2), [ Num(value=3), [ Num(value=4), [ Num(value=5), Num(value=6), Num(value=7) ] ] ] ], Num(value=8), Num(value=9) ] ]"
+        }
+        "item zero in [1,2,3] is 1, item 1 is 2, item 2 is 3, item 3 is null" {
+            "1,2,3".toSymbols().parse().NumAt(0) shouldBe 1
+            "1,2,3".toSymbols().parse().NumAt(1) shouldBe 2
+            "1,2,3".toSymbols().parse().NumAt(2) shouldBe 3
+            "1,2,3".toSymbols().parse().NumAt(3) shouldBe null
+        }
+        "[1,[2,[3,[4,[5,6,7]]]],8,9] as a list is 1,2,3,4,5,6,7,8,9" {
+            "[1,[2,[3,[4,[5,6,7]]]],8,9]".toSymbols().parse().toList() shouldBe listOf(1,2,3,4,5,6,7,8,9)
+        }
+        " with 1,2,3 and 2,2,3 they are in the right order because 1 < 2  " {
+            val left = "1,2,3".toSymbols().parse()
+            val right = "2,2,3".toSymbols().parse()
+//            compare(left, right) shouldBe true
+        }
+        "Compare [1,1,3,1,1] vs [1,1,5,1,1] should be true" {
+            val left = "[1,1,3,1,1]".toSymbols().parse()
+            val right = "[1,1,5,1,1]".toSymbols().parse()
+//            compare(left, right) shouldBe true
+        }
+        "Compare [[1],[2,3,4]] vs [[1],4] should be true" {
+            val left = "[[1],[2,3,4]]".toSymbols().parse()
+            val right = "[[1],4]".toSymbols().parse()
+            compare(left, right) shouldBe true
+        }
+        "Compare [9] vs [[8,7,6]] should be false" {
+            val left = "[9]".toSymbols().parse()
+            val right = "[[8,7,6]]".toSymbols().parse()
+            compare(left, right) shouldBe false
+        }
+        "Compare [[4,4],4,4] vs [[4,4],4,4,4] should be true" {
+            val left = "[[4,4],4,4]".toSymbols().parse()
+            val right = "[[4,4],4,4,4]".toSymbols().parse()
+            compare(left, right) shouldBe true
+        }
+        "Compare [7,7,7,7] vs [7,7,7] should be false" {
+            val left = "[7,7,7,7]".toSymbols().parse()
+            val right = "[7,7,7]".toSymbols().parse()
+            compare(left, right) shouldBe false
+        }
+        "Compare [] vs [3] should be true" {
+            val left = "[]".toSymbols().parse()
+            val right = "[3]".toSymbols().parse()
+            compare(left, right) shouldBe true
+        }
+        "Compare [[[]]] vs [[]] should be false" {
+            val left = "[[[]]]".toSymbols().parse()
+            val right = "[[]]".toSymbols().parse()
+            compare(left, right) shouldBe false
+        }
+        "Compare [1,[2,[3,[4,[5,6,7]]]],8,9] vs [1,[2,[3,[4,[5,6,0]]]],8,9] should be false" {
+            val left = "[1,[2,[3,[4,[5,6,7]]]],8,9]".toSymbols().parse()
+            val right = "[1,[2,[3,[4,[5,6,0]]]],8,9]".toSymbols().parse()
+            compare(left, right) shouldBe false
+        }
+        "part one with sample input" {
+            partOne(sampleInput) shouldBe 13
+        }
+        "part one with puzzle input" {
+            partOne(puzzleInput) shouldBe 4734
         }
     })
-    "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+    "Part two " should ({
+        "part two with sample input" {
+            partTwo(sampleInput) shouldBe  140
+        }
+        "part two with puzzle input" {
+            partTwo(puzzleInput) shouldBe  140
         }
     })
 })
