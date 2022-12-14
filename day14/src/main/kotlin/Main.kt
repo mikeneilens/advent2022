5982b5823d299a0c8c9af433cd681305055b78c6
Mike Neilens <mike.neilens@gmail.com>
2022-12-14 23:31:49 +0000

    day14

--- day14/src/main/kotlin/Main.kt
+++ day14/src/main/kotlin/Main.kt
@@ -1,1 +1,74 @@
-fun helloWorld() = "hello world"
+data class Position(val x:Int, val y:Int)
+
+fun String.toPosition() = Position(split(",").first().toInt(),split(",").last().toInt())
+
+
+fun List<String>.popluate(map:MutableMap<Position, Char>) = forEach {it.populate(map)}
+
+fun String.populate(map:MutableMap<Position, Char>) {
+    if (split(" -> ").size < 2) return
+    val (p1, p2) = split(" -> ").take(2).map(String::toPosition)
+
+    (minOf(p1.y,p2.y)..maxOf(p1.y,p2.y)).forEach{ y ->
+        (minOf(p1.x,p2.x)..maxOf(p1.x,p2.x)) .forEach { x ->
+            map[Position(x,y)] = '#'
+        }
+    }
+    drop(indexOfFirst { it == '>' } + 2).populate(map)
+}
+
+fun Map<Position, Char>.firstBelow(position: Position): Position? =
+    toList().filter { it.first.x == position.x && it.first.y > position.y }.sortedBy { it.first.y }.firstOrNull()?.first
+
+fun MutableMap<Position, Char>.dropSand(sand:Position, maxY:Int):Position? {
+    if (this[sand] != null) return null
+    var x = sand.x
+    var y = sand.y
+    while (y < maxY  && ( !containsKey(Position(x, y + 1)) || !containsKey(Position(x -1, y + 1)) || !containsKey(Position(x + 1, y + 1))  )) {
+        if (!containsKey(Position(x , y + 1))) y++
+        else if (!containsKey(Position(x - 1 , y + 1))) { x--; y++}
+        else if (!containsKey(Position(x  + 1, y + 1))) { x++; y++}
+    }
+    if (y >= maxY) return null
+    this[Position(x, y)] = 'O'
+    return Position(x, y)
+}
+
+fun MutableMap<Position, Char>.process(maxY:Int):Map<Position,Char> {
+    var sand = dropSand(Position(500,0), maxY)
+    while (sand != null) {
+        val result  = dropSand(Position(500,0), maxY)
+        sand = result
+    }
+    return this
+}
+
+fun MutableMap<Position, Char>.toText():String{
+    var s=""
+    (0..10).forEach{y->
+        (480..520).forEach{x ->
+            if (this[Position(x,y)] != null) s += this[Position(x,y)]
+            else s += "."
+        }
+        s += "\n"
+    }
+    return s
+}
+
+fun partOne2(data:List<String>):Int {
+    val map = mutableMapOf<Position, Char>()
+    data.popluate(map)
+    val maxY = map.toList().maxOf { it.first.y }
+    map.process(maxY)
+    return map.toList().count{it.second == 'O'}
+}
+
+fun partTwo2(data:List<String>):Int {
+    val map = mutableMapOf<Position, Char>()
+    data.popluate(map)
+    val maxY = map.toList().maxOf { it.first.y }
+    val maxx = map.toList().maxOf { it.first.x }
+    ((-maxx * 2)..(+maxx * 2)).forEach{ x -> map[Position(x,maxY + 2)] = '#' }
+    map.process(maxY + 2)
+    return map.toList().count{it.second == 'O'}
+}
--- day14/src/test/kotlin/MainTest.kt
+++ day14/src/test/kotlin/MainTest.kt
@@ -2,14 +2,24 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        498,4 -> 498,6 -> 496,6
+        503,4 -> 502,4 -> 502,9 -> 494,9
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part one with sample input" {
+              partOne2(sampleInput) shouldBe 24
+        }
+        "part one with puzzle input" {
+              partOne2(puzzleInput) shouldBe 1330
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two with sample input" {
+              partTwo2(sampleInput) shouldBe 93
+        }
+        "part two with puzzle input" {
+              partTwo2(puzzleInput) shouldBe 26139
         }
     })
 })
