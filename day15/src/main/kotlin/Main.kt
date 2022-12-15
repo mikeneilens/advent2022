9846dfc831c28b03ac1ac2608118eaf371976800
Mike Neilens <mike.neilens@gmail.com>
2022-12-15 15:37:03 +0000

    day15 part one

--- day15/src/main/kotlin/Main.kt
+++ day15/src/main/kotlin/Main.kt
@@ -1,1 +1,56 @@
+import kotlin.math.abs
+
 fun helloWorld() = "hello world"
+
+data class Position(val x:Int, val y:Int)
+
+data class Sensor(val position:Position, val beacon:Position) {
+    val manhattenSize = abs(position.x - beacon.x) + abs(position.y - beacon.y)
+    fun xRange(y:Int):IntRange? {
+        val span = manhattenSize - abs(y - position.y)
+        return if (span >= 0) (position.x - span)..(position.x + span) else null
+    }
+    fun yRange(x:Int):IntRange? {
+        val span = manhattenSize - abs(x - position.x)
+        return if (span >= 0) (position.y - span)..(position.y + span) else null
+    }
+}
+
+fun List<String>.toSensors() = map(String::toSensor)
+
+fun String.toSensor():Sensor {
+    val chunks = split("=")
+    val sensorX = chunks[1].removeSuffix(", y").toInt()
+    val sensorY = chunks[2].removeSuffix(": closest beacon is at x").toInt()
+    val beaconX = chunks[3].removeSuffix(", y").toInt()
+    val beaconY = chunks[4].toInt()
+    return Sensor(Position(sensorX,sensorY), Position(beaconX,beaconY))
+}
+
+fun List<Sensor>.sensorsInRange(y:Int) = filter {val yRange = it.yRange(it.position.x);  yRange != null && y in yRange}
+fun List<Sensor>.positionsOfBeaconsOnRow(y:Int) = filter {it.beacon.y == y}.map{it.beacon.x}.toSet()
+
+fun consolidateRanges(range1:IntRange, range2:IntRange):List<IntRange> =
+    if (range2.start > range1.last) listOf(range1, range2)
+    else listOf(range1.start..maxOf(range1.last, range2.last))
+
+fun List<IntRange>.consolidateRanges():List<IntRange> {
+    val sorted = sortedWith(compareBy ({it.first},{it.last}))
+    return sorted.drop(1)
+        .fold(listOf(sorted.first())){ list, range ->
+            val consolidated:List<IntRange> = consolidateRanges(list.first(), range)
+            println("$range ${list.first()} $consolidated")
+            if (consolidated.size == 1) list.dropLast(1) + consolidated else list + listOf(consolidated[1])
+        }
+}
+
+fun partOne(data:List<String>,row:Int = 10):Int {
+    val sensors = data.toSensors()
+    val scannedPositionsOnRow = sensors.sensorsInRange(row)
+    val consolidated = scannedPositionsOnRow.map{it.xRange(row) ?: 0..-1}.consolidateRanges()
+    println(consolidated)
+    println(consolidated.sumOf { (it.last - it.first + 1) })
+    val  positionOfBeaconsOnRow = sensors.positionsOfBeaconsOnRow(row)
+
+    return (consolidated.sumOf { it.count() } - positionOfBeaconsOnRow.size)
+}
--- day15/src/test/kotlin/MainTest.kt
+++ day15/src/test/kotlin/MainTest.kt
@@ -2,9 +2,77 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+
+    val sampleInput = """
+        Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        Sensor at x=9, y=16: closest beacon is at x=10, y=16
+        Sensor at x=13, y=2: closest beacon is at x=15, y=3
+        Sensor at x=12, y=14: closest beacon is at x=10, y=16
+        Sensor at x=10, y=20: closest beacon is at x=10, y=16
+        Sensor at x=14, y=17: closest beacon is at x=10, y=16
+        Sensor at x=8, y=7: closest beacon is at x=2, y=10
+        Sensor at x=2, y=0: closest beacon is at x=2, y=10
+        Sensor at x=0, y=11: closest beacon is at x=2, y=10
+        Sensor at x=20, y=14: closest beacon is at x=25, y=17
+        Sensor at x=17, y=20: closest beacon is at x=21, y=22
+        Sensor at x=16, y=7: closest beacon is at x=15, y=3
+        Sensor at x=14, y=3: closest beacon is at x=15, y=3
+        Sensor at x=20, y=1: closest beacon is at x=15, y=3
+    """.trimIndent().split("\n")
+
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse Sensor at x=2, y=18: closest beacon is at x=-2, y=15" {
+            "Sensor at x=2, y=18: closest beacon is at x=-2, y=15".toSensor() shouldBe Sensor( Position(2,18), Position(-2,15) )
+        }
+        "parse sensors in sample input" {
+            sampleInput.toSensors().size shouldBe 14
+            sampleInput.toSensors().first() shouldBe Sensor( Position(2,18), Position(-2,15) )
+            sampleInput.toSensors().last() shouldBe Sensor( Position(20,1), Position(15,3) )
+        }
+        "manhatten size of Sensor at x=8, y=7: closest beacon is at x=2, y=10, is 9 " {
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10".toSensor().manhattenSize shouldBe 9
+        }
+        "xRange on row 7 for Sensor at x=8, y=7: closest beacon is at x=2, y=10, y=15 is -1..17 " {
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10".toSensor().xRange(7) shouldBe -1..17
+        }
+        "xRange on row 8 for Sensor at x=8, y=7: closest beacon is at x=2, y=10, y=15 is 0..16 " {
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10".toSensor().xRange(8) shouldBe 0..16
+        }
+        "xRange on row 16 for Sensor at x=8, y=7: closest beacon is at x=2, y=10, y=15 is 8..8 " {
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10".toSensor().xRange(16) shouldBe 8..8
+        }
+        "xRange on row 17 for Sensor at x=8, y=7: closest beacon is at x=2, y=10, y=15 is null " {
+            "Sensor at x=8, y=7: closest beacon is at x=2, y=10".toSensor().xRange(17) shouldBe null
+        }
+        "xRange on row 10 for Sensor at x=20, y=14: closest beacon is at x=25, y=17 is " {
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17".toSensor().manhattenSize shouldBe 8
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17".toSensor().xRange(14) shouldBe 12..28
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17".toSensor().xRange(13) shouldBe 13..27
+            "Sensor at x=20, y=14: closest beacon is at x=25, y=17".toSensor().xRange(10) shouldBe 16..24
+        }
+        "using the sampe input the sensors in range on line 10 are" {
+            sampleInput.toSensors().sensorsInRange(10) shouldBe listOf(
+                Sensor(position=Position(x=12, y=14), beacon=Position(x=10, y=16)),
+                Sensor(position=Position(x=8, y=7), beacon=Position(x=2, y=10)),
+                Sensor(position=Position(x=2, y=0), beacon=Position(x=2, y=10)),
+                Sensor(position=Position(x=0, y=11), beacon=Position(x=2, y=10)),
+                Sensor(position=Position(x=20, y=14), beacon=Position(x=25, y=17)),
+                Sensor(position=Position(x=16, y=7), beacon=Position(x=15, y=3)))
+        }
+        "using the sample input the number of beacons or row 10 is 1" {
+            sampleInput.toSensors().positionsOfBeaconsOnRow(10).size shouldBe 1
+        }
+        "consolidating range 1..10 and 4..15 gives 1..15, consolidating 1..10 and 20..30 gives 1..10, 20..30" {
+            consolidateRanges(1..10, 4..15) shouldBe listOf(1..15)
+        }
+        "consolidating a list of ranges gives a shorter list of ranges" {
+            listOf(-2..2, 2..2, 2..14, 12..12, 14..18, 16..24).consolidateRanges() shouldBe listOf(-2..24)
+        }
+        "using the sample input and row 10 part one is 26" {
+            partOne(sampleInput, 10) shouldBe 26
+        }
+        "using the puzzle input and row 2000000 part one is 5083287" {
+            partOne(puzzleInput, 2000000) shouldBe 5083287
         }
     })
     "Part two" should ({
