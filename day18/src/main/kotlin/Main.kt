67680e9ed274421513d8897f9694bf4b931dee34
Mike Neilens <mike.neilens@gmail.com>
2022-12-18 12:33:30 +0000

    day 18 part one

--- day18/src/main/kotlin/Main.kt
+++ day18/src/main/kotlin/Main.kt
@@ -1,1 +1,26 @@
 fun helloWorld() = "hello world"
+
+data class Position( val x:Int, val y:Int, val z:Int) {
+    operator fun plus(other:Position):Position = Position(x + other.x, y + other.y, z + other.z)
+}
+fun Cube(x:Int, y:Int, z:Int) = Cube(Position(x,y,z))
+
+data class Cube(
+    val p:Position,
+    val adjacentCubes:Set<Position> = setOf(
+        p + Position(1,0,0),p + Position(-1,0,0),
+        p + Position(0,1,0),p + Position(0,-1,0),
+        p + Position(0,0,1),p + Position(0,0,-1),
+    ))
+
+fun List<String>.toCubes() = map{ Cube(it.toDimension(0),it.toDimension(1),it.toDimension(2))}
+fun String.toDimension(n:Int) = split(",")[n].toInt()
+
+fun List<Cube>.unconnectedSides(cube:Cube) = cube.adjacentCubes.count{adjacent -> adjacent!in positionOfOtherCubes(cube)}
+
+private fun List<Cube>.positionOfOtherCubes(cube: Cube): List<Position> = filter { it != cube }.map { it.p }
+
+fun partOne(data:List<String>):Int {
+    val cubes = data.toCubes()
+    return cubes.sumOf{cube -> cubes.unconnectedSides(cube)}
+}
--- day18/src/test/kotlin/MainTest.kt
+++ day18/src/test/kotlin/MainTest.kt
@@ -1,10 +1,53 @@
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.shouldBe
+
+val sampleInput = """
+    2,2,2
+    1,2,2
+    3,2,2
+    2,1,2
+    2,3,2
+    2,2,1
+    2,2,3
+    2,2,4
+    2,2,6
+    1,2,5
+    3,2,5
+    2,1,5
+    2,3,5
+""".trimIndent().split("\n")
 
 class MainTest: WordSpec( {
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse sample input into cubes" {
+            val cubes = sampleInput.toCubes()
+            cubes.size shouldBe 13
+            cubes[0] shouldBe Cube(Position(2,2,2))
+            cubes[12] shouldBe Cube(Position(2,3,5))
+        }
+        "cubes adjacent to Cube(1,1,1) should be " {
+            val cube = Cube(1,1,1)
+            cube.adjacentCubes shouldContain  Position(0,1,1)
+            cube.adjacentCubes shouldContain  Position(2,1,1)
+            cube.adjacentCubes shouldContain  Position(1,0,1)
+            cube.adjacentCubes shouldContain  Position(1,2,1)
+            cube.adjacentCubes shouldContain  Position(1,1,0)
+            cube.adjacentCubes shouldContain  Position(1,1,2)
+        }
+        "no of sides not connected for cube(1,1,1) in [cube(1,1,1), cube(2,1,1)] "{
+            val cubes = listOf(Cube(1,1,1), Cube(2,1,1))
+            cubes.unconnectedSides(Cube(1,1,1)) shouldBe 5
+        }
+        "no of sides not connected for cube(2,1,1) in [cube(1,1,1), cube(2,1,1)] "{
+            val cubes = listOf(Cube(2,1,1), Cube(2,1,1))
+            cubes.unconnectedSides(Cube(1,1,1)) shouldBe 5
+        }
+        "part one with sample input is 64" {
+            partOne(sampleInput) shouldBe 64
+        }
+        "part one with puzzle input is 4400" {
+            partOne(puzzleInput) shouldBe 4400
         }
     })
     "Part two" should ({
