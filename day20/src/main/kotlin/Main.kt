556865c2903ba8bb22806eeaba88aff89db10443
Mike Neilens <mike.neilens@gmail.com>
2022-12-23 18:16:51 +0000

    day20

--- day20/src/main/kotlin/Main.kt
+++ day20/src/main/kotlin/Main.kt
@@ -1,1 +1,54 @@
-fun helloWorld() = "hello world"
+class LinkedList (val value:Long, var next:LinkedList? = null, var prev:LinkedList? = null) {
+
+    fun move(size:Long) {
+        remove()
+        insert(nodeToInsertAfter = nodeToInsertAfter(size))
+    }
+
+    fun remove() {
+        prev?.next = next
+        next?.prev = prev
+    }
+
+    fun insert(nodeToInsertAfter:LinkedList?) {
+        prev = nodeToInsertAfter
+        next = nodeToInsertAfter?.next
+        nodeToInsertAfter?.next = this
+        next?.prev = this
+    }
+
+    fun nodeToInsertAfter(size: Long): LinkedList? =
+        if (value >= 0) prev?.after(value % size) else prev?.after(size + (value % size))
+
+    fun after(n:Long ):LinkedList = if (n == 0L) this else next?.after(n - 1) ?: this
+}
+
+fun String.toLinkedList(listOfNodes:MutableList<LinkedList> = mutableListOf(), multiplier:Int = 1):LinkedList {
+    val numbers = split(", ").map{it.toLong() * multiplier}
+    val firstNode = LinkedList(numbers.first())
+    listOfNodes.add(firstNode)
+    numbers.drop(1).fold(firstNode){ ll, number ->
+        val newItem = LinkedList(number, firstNode, listOfNodes.last())
+        ll.next = newItem
+        listOfNodes.add(newItem)
+        newItem
+    }
+    firstNode.prev = listOfNodes.last()
+    return firstNode
+}
+
+fun List<LinkedList>.itemWithValue(n:Long) = first{it.value == n}
+
+fun partOne(data:String, noOfRepetitions:Int = 1, multiplier:Int = 1):Long {
+    val listOfNodes = mutableListOf<LinkedList>()
+    data.toLinkedList(listOfNodes, multiplier)
+    repeat(noOfRepetitions) {
+        listOfNodes.forEach {
+            it.move(listOfNodes.size.toLong() - 1)
+        }
+    }
+    val zeroNode = listOfNodes.itemWithValue(0)
+    return (1L..3L).sumOf{zeroNode.after(it * 1000).value}
+}
+
+fun partTwo(data:String):Long = partOne(data, noOfRepetitions = 10, multiplier = 811589153)
--- day20/src/test/kotlin/MainTest.kt
+++ day20/src/test/kotlin/MainTest.kt
@@ -1,15 +1,67 @@
 import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
-class MainTest: WordSpec( {
+class Main3Test: WordSpec( {
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "adding sample input to linkedlist" {
+            val list = mutableListOf<LinkedList>()
+            val linkedList = "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.size shouldBe 7
+            linkedList.toList() shouldBe list
         }
+        "move first item " {
+            val list = mutableListOf<LinkedList>()
+            "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.first().move(list.size.toLong() - 1)
+            list.first().toList().map{it?.value} shouldBe listOf(1,-3,3,-2,0,4,2)
+        }
+        "move 2nd item " {
+            val list = mutableListOf<LinkedList>()
+            "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.take(2).forEach { it.move(list.size.toLong() - 1) }
+            list.first().toList().map{it?.value} shouldBe listOf(1,-3,2,3,-2,0,4)
+        }
+        "move 3rd item " {
+            val list = mutableListOf<LinkedList>()
+            "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.take(3).forEach { it.move(list.size.toLong() - 1) }
+            list.first().toList().map{it?.value} shouldBe listOf(1,2,3,-2,-3,0,4)
+        }
+        "move 7 items " {
+            val list = mutableListOf<LinkedList>()
+            "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.take(7).forEach { it.move(list.size.toLong() - 1) }
+            list.first().toList().map{it?.value} shouldBe listOf(1, 2, -3, 4, 0, 3, -2)
+        }
+        "get item 3 positions after zero should be 2" {
+            val list = mutableListOf<LinkedList>()
+            "1, 2, -3, 3, -2, 0, 4".toLinkedList(list)
+            list.itemWithValue(0).after(3).value shouldBe 2
+        }
+
+        "part one with sample input" {
+            partOne("1, 2, -3, 3, -2, 0, 4") shouldBe 3
+        }
+        "part one with puzzle input" {
+            partOne(puzzleInput) shouldBe 23321
+        }
+
+
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two with sample input" {
+            partTwo("1, 2, -3, 3, -2, 0, 4") shouldBe 1623178306
+        }
+        "part two with puzzle input" {
+            partTwo(puzzleInput) shouldBe 1428396909280
         }
     })
 })
+
+fun LinkedList.toList():List<LinkedList?> {
+    val list = mutableListOf<LinkedList?>(this)
+    while (list.last()?.next != this) {
+        list.add(list.last()?.next)
+    }
+    return list
+}
