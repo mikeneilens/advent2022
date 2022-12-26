caf33ec0d79af89d8a110cf4b6c867022076a49b
Mike Neilens <mike.neilens@gmail.com>
2022-12-26 11:31:27 +0000

    day21

--- day21/src/main/kotlin/Main.kt
+++ day21/src/main/kotlin/Main.kt
@@ -1,1 +1,84 @@
-fun helloWorld() = "hello world"
+val operMap = mapOf<String, (Long, Long)->Long>(
+    "+" to { v1, v2 -> v1 + v2 },
+    "-" to { v1, v2 -> v1 - v2 },
+    "*" to { v1, v2 -> v1 * v2 },
+    "/" to { v1, v2 -> v1 / v2 }
+    )
+
+sealed class Job {
+    class LoneNumber(val value: Long) : Job()
+    class MathOperation(val node1: Node, val node2: Node, val operation: (Long, Long) -> Long) : Job()
+    object None : Job()
+}
+
+class Node(var job:Job) {
+    fun total():Long {
+        if (job is Job.LoneNumber) return (job as Job.LoneNumber).value
+        else {
+            val mathOperation = (job as Job.MathOperation)
+            return mathOperation.operation( mathOperation.node1.total(), mathOperation.node2.total())
+        }
+    }
+}
+
+fun List<String>.parse():Map<String, Node> {
+    val nodeRegister = mutableMapOf<String, Node>()
+    forEach { line ->
+        val node = if (line.jobName() in nodeRegister)
+            nodeRegister.getValue(line.jobName())
+        else {
+            Node(Job.None)
+        }
+        nodeRegister[line.jobName()] = node
+        if (line.lineIsANumber()) {
+            node.job = Job.LoneNumber(line.param(1).toLong())
+        } else {
+            node.job = line.getMathOperations(nodeRegister, node)
+        }
+    }
+    return nodeRegister
+}
+
+fun String.getMathOperations(nodeRegister: MutableMap<String, Node>, node: Node) =
+    Job.MathOperation(getNodeForParameter(1, nodeRegister), getNodeForParameter(3, nodeRegister), oper())
+
+fun String.getNodeForParameter(n:Int, nodeRegister:MutableMap<String, Node>):Node {
+    val node = if (param(n) in nodeRegister)
+        nodeRegister.getValue(param(n))
+    else {
+        Node(Job.None)
+    }
+    nodeRegister[param(n)] = node
+    return node
+}
+
+fun String.jobName() = split(":")[0]
+fun String.lineIsANumber() = param(1).toLongOrNull() != null
+fun String.param(n:Int) = split(" ")[n]
+fun String.oper() = operMap.getValue( split(" ")[2])
+
+
+fun partOne(data:List<String>):Long {
+    val nodeMap = data.parse()
+    val root = nodeMap.getValue("root")
+    return root.total()
+}
+
+fun partTwo(data:List<String>):Long {
+    val nodes = puzzleInput.parse()
+    val zfhnTotal = nodes.getValue("zfhn").total()
+    val humnNode = nodes.getValue("humn")
+    val jgtbNode =  nodes.getValue("jgtb")
+    return findNumber(zfhnTotal, jgtbNode, humnNode, 3757270000000L,3757272500000L)
+}
+
+fun findNumber(target:Long, otherNode:Node, human:Node, min:Long, max:Long):Long {
+    val testValue = min + (max - min)/2
+    human.job = Job.LoneNumber(testValue)
+    val total = otherNode.total()
+    if (target == total) return testValue
+    if (target < total)
+        return findNumber(target, otherNode, human, testValue, max )
+    else
+        return findNumber(target, otherNode, human, min, testValue )
+}
--- day21/src/test/kotlin/MainTest.kt
+++ day21/src/test/kotlin/MainTest.kt
@@ -2,14 +2,48 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+        root: pppw + sjmn
+        dbpl: 5
+        cczh: sllz + lgvd
+        zczc: 2
+        ptdq: humn - dvpt
+        dvpt: 3
+        lfqf: 4
+        humn: 5
+        ljgn: 2
+        sjmn: drzm * dbpl
+        sllz: 4
+        pppw: cczh / lfqf
+        lgvd: ljgn * ptdq
+        drzm: hmdt - zczc
+        hmdt: 32
+    """.trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse sample input into nodes" {
+            val nodes = sampleInput.parse()
+            (nodes["root"]?.job is Job.MathOperation) shouldBe true
+            val rootNode = nodes["root"]?.job as Job.MathOperation
+            rootNode.operation(1,2) shouldBe 3
+            rootNode.node1 shouldBe nodes.getValue("pppw")
+            rootNode.node2 shouldBe nodes.getValue("sjmn")
+        }
+        "calculate total for sample input as 152" {
+            val nodes = sampleInput.parse()
+            val root = nodes.getValue("root")
+            root.total() shouldBe 152
+        }
+        "partone using sample input is 152" {
+            partOne(sampleInput) shouldBe 152
+        }
+        "partone using puzzle input is 152" {
+            partOne(puzzleInput) shouldBe 309248622142100
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two using puzzle input" {
+            partTwo(puzzleInput) shouldBe 3757272361782
+            //nb. 3757272361783 is also correct!
         }
     })
 })
