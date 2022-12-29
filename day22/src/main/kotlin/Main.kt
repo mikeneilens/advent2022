5fc8e4c9220405103bfff64f94c82b340ff80bb5
Mike Neilens <mike.neilens@gmail.com>
2022-12-29 16:28:04 +0000

    day22

--- day22/src/main/kotlin/Main.kt
+++ day22/src/main/kotlin/Main.kt
@@ -1,1 +1,181 @@
-fun helloWorld() = "hello world"
+sealed class Instruction {
+    data class Move(val distance: Int):Instruction()
+    object RotateLeft:Instruction()
+    object RotateRight:Instruction()
+}
+
+data class Position(val x:Int, val y:Int) {
+    operator fun plus(other: Position) = Position(x + other.x, y + other.y)
+}
+
+data class Tile(val type:Char, val next:MutableMap<String, Position> = mutableMapOf(), val nextDirection:MutableMap<String, String> = mutableMapOf() )
+
+fun List<String>.parseToInstructions() = last().parseToInstructions()
+
+tailrec fun String.parseToInstructions(instructions: List<Instruction> = listOf()): List<Instruction>  =
+    when (true) {
+        isEmpty() -> instructions
+        (first() == 'L') -> drop(1).parseToInstructions(instructions + Instruction.RotateLeft)
+        (first() == 'R') -> drop(1).parseToInstructions(instructions + Instruction.RotateRight)
+        else -> {
+            val numberString = takeWhile { it != 'R' && it != 'L' }
+            val move = Instruction.Move(numberString.toInt() )
+            drop(numberString.length ).parseToInstructions(instructions + move)
+        }
+    }
+
+fun List<String>.createMap():Map<Position, Tile> =
+    flatMapIndexed { y, line ->
+        line.mapIndexedNotNull{ x, char ->
+            if (char == '.' || char == '#') Pair(Position(x +1,y + 1), Tile(char)) else null
+        }
+    }.toMap()
+
+
+val rightMap = mapOf("E" to "S", "S" to "W", "W" to "N", "N" to "E" )
+val leftMap = mapOf("E" to "N", "N" to "W", "W" to "S", "S" to "E" )
+val offsets = mapOf("E" to Position(1,0), "W" to Position(-1,0), "S" to Position(0,1), "N" to Position(0,-1))
+
+data class Person(val tile:Tile, val direction:String, val map:Map<Position, Tile>) {
+    fun turnRight() = copy( direction =   rightMap[direction] ?: "E")
+    fun turnLeft() = copy( direction = leftMap[direction] ?: "E" )
+
+    fun move(qty:Int):Person {
+        if (qty == 0) return this
+        val nextTile = map[tile.next[direction]]
+        if (nextTile == null || nextTile.type == '#') return this
+        val newDirection = tile.nextDirection.getValue(direction)
+        return copy(tile = nextTile, direction = newDirection).move(qty - 1)
+    }
+
+    fun process(instructions: List<Instruction>) =
+         instructions.fold(this) { newPerson, instruction ->
+            when (instruction) {
+                is Instruction.RotateLeft -> newPerson.turnLeft()
+                is Instruction.RotateRight -> newPerson.turnRight()
+                is Instruction.Move -> newPerson.move(instruction.distance)
+            }
+        }
+    fun positionOnMap() = map.filter { it.value == tile }.keys.first()
+}
+
+fun Map<Position, Tile>.toPerson() = Person(getValue(Position(keys.filter{it.y == 1 }.minOf { it.x },1)),"E", this)
+
+fun partOne(data:List<String>, edgeMap:Map<Line, Line>):Int {
+    val instructions = data.parseToInstructions()
+    val map = prepareMap(data, edgeMap)
+    val movedPerson = map.toPerson().process(instructions)
+    val (x,y ) = movedPerson.positionOnMap()
+    return y * 1000 + 4 * x + rightMap.keys.indexOfFirst { it == movedPerson.direction }
+}
+
+//part two
+data class Line(val start:Position, val end:Position, val perpendicularDirection:String = "") {
+    fun positions():List<Position> {
+        val incX = if (start.x < end.x) 1 else if (start.x > end.x) -1 else 0
+        val incY = if (start.y < end.y) 1 else if (start.y > end.y) -1 else 0
+        var (x,y) = start
+        val result = mutableListOf(start)
+        while (x != end.x || y != end.y) {
+            x += incX; y += incY
+            result.add(Position(x,y))
+        }
+        return result
+    }
+    fun isVertical() = start.x == end.x
+}
+
+fun prepareMap(data:List<String>, edgeMap:Map<Line, Line>):Map<Position, Tile> =
+    data.createMap().toMap().apply{
+        updateNextPositionOfTiles()
+        updateEdges(edgeMap)}
+
+fun Map<Position, Tile>.updateNextPositionOfTiles() {
+    forEach { (position, tile) ->
+        offsets.forEach { (direction, offset) ->
+            if ( contains(position + offset)) {
+                tile.next[direction] = position + offset
+                tile.nextDirection[direction] = direction
+            }
+        }
+    }
+}
+
+fun Map<Position, Tile>.updateEdges(edgeMap: Map<Line, Line>) {
+    edgeMap.forEach { (fromLine, toLine) -> updateEdge(fromLine, toLine) }
+}
+
+fun Map<Position, Tile>.updateEdge(fromLine:Line, toLine:Line) {
+    val direction = if (fromLine.isVertical()) {
+        if (getValue(fromLine.start).next.containsKey("E")) "W" else "E"
+    } else {
+        if (getValue(fromLine.start).next.containsKey("S")) "N" else "S"
+    }
+    updateNextPositionsOnEdge(fromLine, toLine, direction)
+}
+
+fun Map<Position,Tile>.updateNextPositionsOnEdge(fromLine:Line, toLine:Line, direction:String) {
+    fromLine.positions().zip(toLine.positions()).forEach { (fromPosition, toPosition) ->
+        getValue(fromPosition).next[direction] = toPosition
+        getValue(fromPosition).nextDirection[direction] = toLine.perpendicularDirection
+    }
+}
+
+fun partTwo(data:List<String>):Int = partOne(data, edgeMapP2)
+
+
+//data describing the edges of the layout.
+val a = Position(51,1)
+val b = Position(100,1)
+val c = Position(101,1)
+val d = Position(150,1)
+val e = Position(150,50)
+val f = Position(101,50)
+val g = Position(51,50)
+val h = Position(51,51)
+val j = Position(100,51)
+val k = Position(100,100)
+val i = Position(51,100)
+val l = Position(1,101)
+val m = Position(50,101)
+val o = Position(100,101)
+val n = Position(1,150)
+val q = Position(51,150)
+val p = Position(100,150)
+val r = Position(1,151)
+val s = Position(50,151)
+val t = Position(1,200)
+val u = Position(50,200)
+
+val edgeMapP2 = mapOf(
+    Line(a,b) to Line(r,t,"E"),
+    Line(c,d) to Line(t,u,"N"),
+    Line(d,e) to Line(p,o,"W"),
+    Line(e,f) to Line(k,j,"W"),
+    Line(j,k) to Line(f,e, "N"),
+    Line(o,p) to Line(e,d,"W"),
+    Line(p,q) to Line(u,s,"W"),
+    Line(s,u) to Line(q,p, "N"),
+    Line(t,u) to Line(c,d,"S"),
+    Line(r,t) to Line(a,b,"S"),
+    Line(l,n) to Line(g,a,"E"),
+    Line(l,m) to Line(h,i,"E"),
+    Line(i,h) to Line(m,l,"S"),
+    Line(a,g) to Line(n,l,"E"),
+)
+val edgeMapP1 = mapOf(
+    Line(a,b) to Line(q,p,"N"),
+    Line(c,d) to Line(f,e,"N"),
+    Line(d,e) to Line(a,g,"E"),
+    Line(e,f) to Line(d,c,"S"),
+    Line(j,k) to Line(h,i, "E"),
+    Line(o,p) to Line(l,n,"E"),
+    Line(p,q) to Line(b,a,"S"),
+    Line(s,u) to Line(r,t, "E"),
+    Line(t,u) to Line(l,m,"S"),
+    Line(r,t) to Line(s,u,"W"),
+    Line(l,n) to Line(o,p,"W"),
+    Line(l,m) to Line(t,u,"N"),
+    Line(i,h) to Line(k,j,"W"),
+    Line(a,g) to Line(d,e,"W"),
+)
--- day22/src/test/kotlin/MainTest.kt
+++ day22/src/test/kotlin/MainTest.kt
@@ -2,14 +2,130 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleInput = """
+________...#
+________.#..
+________#...
+________....
+...#.......#
+........#...
+..#....#....
+..........#.
+________...#....
+________.....#..
+________.#......
+________......#.
+
+10R5L5R10L4R5L5""".trimIndent().split("\n")
+
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parse instructions corrctly" {
+            val instructions = sampleInput.parseToInstructions()
+            instructions.size shouldBe 13
+            instructions[0] shouldBe Instruction.Move(10)
+            instructions[1] shouldBe Instruction.RotateRight
+            instructions[2] shouldBe Instruction.Move(5)
+            instructions[3] shouldBe Instruction.RotateLeft
+            instructions[4] shouldBe Instruction.Move(5)
+            instructions[5] shouldBe Instruction.RotateRight
+            instructions[6] shouldBe Instruction.Move(10)
+            instructions[7] shouldBe Instruction.RotateLeft
+            instructions[8] shouldBe Instruction.Move(4)
+            instructions[9] shouldBe Instruction.RotateRight
+            instructions[10] shouldBe Instruction.Move(5)
+            instructions[11] shouldBe Instruction.RotateLeft
+            instructions[12] shouldBe Instruction.Move(5)
+        }
+        "create map" {
+            val map = sampleInput.createMap()
+            map[Position(9,1)]?.type shouldBe '.'
+            map[Position(10,1)]?.type shouldBe '.'
+            map[Position(11,1)]?.type shouldBe '.'
+            map[Position(12,1)]?.type shouldBe '#'
+            map[Position(1,5)]?.type shouldBe '.'
+            map[Position(2,5)]?.type shouldBe '.'
+            map[Position(3,5)]?.type shouldBe '.'
+            map[Position(4,5)]?.type shouldBe '#'
+        }
+
+        "turn person right" {
+            val person = Person(Tile('.'), "E", mapOf<Position, Tile>())
+            person.turnRight().direction shouldBe "S"
+            person.turnRight().turnRight().direction shouldBe "W"
+            person.turnRight().turnRight().turnRight().direction shouldBe "N"
+            person.turnRight().turnRight().turnRight().turnRight().direction shouldBe "E"
+        }
+        "turn person left" {
+            val person = Person(Tile('.'), "E", mapOf<Position, Tile>())
+            person.turnLeft().direction shouldBe "N"
+            person.turnLeft().turnLeft().direction shouldBe "W"
+            person.turnLeft().turnLeft().turnLeft().direction shouldBe "S"
+            person.turnLeft().turnLeft().turnLeft().turnLeft().direction shouldBe "E"
+        }
+        "move person 1 right" {
+            val map = sampleInput.createMap()
+            val person = Person(map.getValue(Position(9,1)), "E", map)
+            val moveddPerson = person.move(1)
+            moveddPerson.tile shouldBe map[Position(10,1)]
+        }
+        "move person 3 right so that it is blocked" {
+            val map = sampleInput.createMap()
+            val person = Person(map.getValue(Position(9,1)), "E", map)
+            val moveddPerson = person.move(3)
+            moveddPerson.tile shouldBe map[Position(11,1)]
+        }
+        "part one with puzzle input" {
+            partOne(puzzleInput, edgeMapP1) shouldBe 106094
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "positions for a line" {
+            Line(Position(1,1), Position(4,1)).positions() shouldBe listOf(Position(1,1),Position(2,1),Position(3,1),Position(4,1))
+            Line(Position(4,1), Position(1,1)).positions() shouldBe listOf(Position(4,1),Position(3,1),Position(2,1),Position(1,1))
+            Line(Position(1,1), Position(1,4)).positions() shouldBe listOf(Position(1,1),Position(1,2),Position(1,3),Position(1,4))
+            Line(Position(1,4), Position(1,1)).positions() shouldBe listOf(Position(1,4),Position(1,3),Position(1,2),Position(1,1))
+        }
+        "update map with all initial next positions for each tile" {
+            val map = puzzleInput.createMap()
+            map.updateNextPositionOfTiles()
+            map.getValue(Position(51,1)).next["E"] shouldBe  Position(52,1)
+            map.getValue(Position(51,1)).next["W"] shouldBe  null
+            map.getValue(Position(51,1)).next["N"] shouldBe  null
+            map.getValue(Position(51,1)).next["S"] shouldBe  Position(51,2)
+        }
+        "update first line of the map with next positions from line that maps to the top line"{
+            val map = puzzleInput.createMap()
+            map.updateNextPositionOfTiles()
+            map.updateEdge(Line(a,b), Line(r,t,"E"))
+            map.getValue(Position(51,1)).next["N"] shouldBe  Position(1,151)
+            map.getValue(Position(52,1)).next["N"] shouldBe  Position(1,152)
+            map.getValue(Position(53,1)).next["N"] shouldBe  Position(1,153)
+            map.getValue(Position(54,1)).next["N"] shouldBe  Position(1,154)
+            map.getValue(Position(51,1)).nextDirection["N"] shouldBe "E"
+            map.updateEdge(Line(a,g), Line(n,l,"E"))
+            map.getValue(Position(51,1)).next["W"] shouldBe  Position(1,150)
+            map.getValue(Position(51,1)).nextDirection["W"] shouldBe  "E"
+        }
+        "udpate all edges" {
+            val map = puzzleInput.createMap()
+            map.updateNextPositionOfTiles()
+            map.updateEdges(edgeMapP2)
+            map.values.all { it.next.size == 4 } shouldBe true
+            map.getValue(Position(51,1)).next["N"] shouldBe  Position(1,151)
+            map.getValue(Position(52,1)).next["N"] shouldBe  Position(1,152)
+            map.getValue(Position(53,1)).next["N"] shouldBe  Position(1,153)
+            map.getValue(Position(54,1)).next["N"] shouldBe  Position(1,154)
+            map.getValue(Position(51,1)).nextDirection["N"] shouldBe "E"
+            map.getValue(Position(1,151)).next["W"] shouldBe  Position(51,1)
+            map.getValue(Position(1,152)).next["W"] shouldBe  Position(52,1)
+            map.getValue(Position(1,153)).next["W"] shouldBe  Position(53,1)
+            map.getValue(Position(1,154)).next["W"] shouldBe  Position(54,1)
+            map.getValue(Position(1,151)).nextDirection["W"] shouldBe  "S"
+            map.getValue(Position(51,1)).next["W"] shouldBe  Position(1,150)
+            map.getValue(Position(51,1)).nextDirection["W"] shouldBe  "E"
+        }
+        "part two with puzzle input" {
+            partTwo(puzzleInput) shouldBe 162038
         }
     })
 })
