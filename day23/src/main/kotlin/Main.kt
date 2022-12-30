b6a658a725931c76380a61658e2d409a427eb94b
Mike Neilens <mike.neilens@gmail.com>
2022-12-30 16:36:12 +0000

    day23

--- day23/src/main/kotlin/Main.kt
+++ day23/src/main/kotlin/Main.kt
@@ -1,1 +1,77 @@
-fun helloWorld() = "hello world"
+data class ElfPosition(val x:Int, val y:Int) {
+    fun proposedMove(elfPositions:ElfPositions, round:Int):ElfPosition {
+        if (noneAnyWhere(elfPositions)) return this
+        val moveIndex = (0..3).firstOrNull { moveSelectors[it.indexFor(round)].positionsAreClear(this, elfPositions) }
+        return if (moveIndex != null) moveSelectors[moveIndex.indexFor(round)].move(this) else this
+    }
+}
+
+fun Int.indexFor(round:Int) = plus(round) % 4
+
+typealias ElfPositions = Set<ElfPosition>
+
+fun List<String>.toElfPositions():ElfPositions {
+    val elfPositions = mutableSetOf<ElfPosition>()
+    forEachIndexed{y, line ->
+        line.forEachIndexed {x, char ->
+            if (char == '#') elfPositions.add(ElfPosition(x,y))
+        }
+    }
+    return elfPositions
+}
+
+fun ElfPosition.noneNorth(others:ElfPositions) = (-1..1).none{others.contains(ElfPosition(x + it, y - 1))}
+fun ElfPosition.noneSouth(others:ElfPositions) = (-1..1).none{others.contains(ElfPosition(x + it, y + 1))}
+fun ElfPosition.noneWest(others:ElfPositions) = (-1..1).none{others.contains(ElfPosition(x - 1, y + it))}
+fun ElfPosition.noneEast(others:ElfPositions) = (-1..1).none{others.contains(ElfPosition(x + 1, y + it))}
+fun ElfPosition.noneAnyWhere(others:ElfPositions) = noneNorth(others) && noneSouth(others) && noneWest(others) && noneEast(others)
+
+data class MoveSelector (val positionsAreClear:ElfPosition.(ElfPositions)-> Boolean, val move:(ElfPosition)->ElfPosition)
+
+val moveSelectors = listOf(
+    MoveSelector (ElfPosition::noneNorth) { p -> ElfPosition(p.x, p.y - 1) },
+    MoveSelector (ElfPosition::noneSouth) { p -> ElfPosition(p.x, p.y + 1) },
+    MoveSelector (ElfPosition::noneWest) { p -> ElfPosition(p.x - 1, p.y) },
+    MoveSelector (ElfPosition::noneEast) { p -> ElfPosition(p.x + 1, p.y) }
+)
+
+data class ProposedMove(val elfPosition: ElfPosition, val newElfPosition: ElfPosition)
+
+fun ElfPositions.findProposedMoves(round:Int, moveRegister:MutableMap<ElfPosition, Int>) =
+    map{ elfPosition ->
+        val proposedElfPosition = elfPosition.proposedMove(this, round)
+        moveRegister[proposedElfPosition] = (moveRegister[proposedElfPosition] ?: 0) + 1
+        ProposedMove(elfPosition, proposedElfPosition)
+    }
+
+fun ElfPositions.newElfPositions(round:Int):ElfPositions {
+    val moveRegister = mutableMapOf<ElfPosition, Int>()
+    val proposedMoves = findProposedMoves(round, moveRegister)
+    return proposedMoves.map{ proposedMove ->
+        if (moveRegister[proposedMove.newElfPosition] == 1) proposedMove.newElfPosition else proposedMove.elfPosition
+    }.toSet()
+}
+
+fun ElfPositions.moveElves(rounds:Int):ElfPositions =
+    (0..(rounds - 1)).fold(this) { elfPosition,  round -> elfPosition.newElfPositions(round) }
+
+fun partOne(data:List<String>):Int {
+    val elfPositions = data.toElfPositions()
+    val newElfPositions = elfPositions.moveElves(10)
+    return newElfPositions.xSize() * newElfPositions.ySize() - newElfPositions.size
+}
+
+fun ElfPositions.xSize() = maxOf { it.x } - minOf { it.x } + 1
+fun ElfPositions.ySize() = maxOf { it.y } - minOf { it.y } + 1
+
+//part two
+tailrec fun ElfPositions.moveElvesUntilNoMovement(round:Int = 0):Int {
+    val newElfPositions = newElfPositions(round )
+    return if (newElfPositions == this) round + 1 else newElfPositions.moveElvesUntilNoMovement(round + 1)
+}
+
+fun partTwo(data:List<String>):Int {
+    val elfPositions = data.toElfPositions()
+    return elfPositions.moveElvesUntilNoMovement()
+}
+
--- day23/src/test/kotlin/MainTest.kt
+++ day23/src/test/kotlin/MainTest.kt
@@ -3,13 +3,96 @@ import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "creating ElfPositions from small sample data" {
+            val elfPositions = smallSample.toElfPositions()
+            elfPositions.contains(ElfPosition(2,1)) shouldBe true
+            elfPositions.contains(ElfPosition(3,1)) shouldBe true
+            elfPositions.contains(ElfPosition(2,2)) shouldBe true
+            elfPositions.contains(ElfPosition(2,4)) shouldBe true
+            elfPositions.contains(ElfPosition(3,4)) shouldBe true
+        }
+        "elf has noneWest if there are not any elves to the west, north west or north east"{
+            val elfPositions = setOf(ElfPosition(3,3), ElfPosition(2,2), ElfPosition(2,4))
+            ElfPosition(3,3).noneWest(elfPositions) shouldBe false
+            ElfPosition(2,3).noneWest(elfPositions) shouldBe true
+            moveSelectors[2].positionsAreClear(ElfPosition(3,3), elfPositions) shouldBe false
+        }
+        "proposed move for elve in small sample data " {
+            val elfPositions = smallSample.toElfPositions()
+            val elfPosition21 = ElfPosition(2,1)
+            val elfPosition31 = ElfPosition(3,1)
+            val elfPosition22 = ElfPosition(2,2)
+            val elfPosition24 = ElfPosition(2,4)
+            val elfPosition34 = ElfPosition(3,4)
+            elfPosition21.proposedMove(elfPositions, 0) shouldBe ElfPosition(2,0)
+            elfPosition31.proposedMove(elfPositions, 0) shouldBe ElfPosition(3,0)
+            elfPosition22.proposedMove(elfPositions, 0) shouldBe ElfPosition(2,3)
+            elfPosition24.proposedMove(elfPositions, 0) shouldBe ElfPosition(2,3)
+            elfPosition34.proposedMove(elfPositions, 0) shouldBe ElfPosition(3,3)
+        }
+        "find proposed moves for all elves in small sample data" {
+            val elfPositions = smallSample.toElfPositions()
+            val elfPosition21 = ElfPosition(2,1)
+            val elfPosition22 = ElfPosition(2,2)
+            val elfPosition24 = ElfPosition(2,4)
+            val moveRegister = mutableMapOf<ElfPosition, Int>()
+            val proposedMoves = elfPositions.findProposedMoves(0, moveRegister)
+            proposedMoves[0] shouldBe ProposedMove(elfPosition21, ElfPosition(2,0))
+            moveRegister[ElfPosition(2,0)] shouldBe 1
+            proposedMoves[2] shouldBe ProposedMove(elfPosition22, ElfPosition(2,3))
+            proposedMoves[3] shouldBe ProposedMove(elfPosition24, ElfPosition(2,3))
+            moveRegister[ElfPosition(2,3)] shouldBe 2
+        }
+        "find new elf positions for all elves in small sample data" {
+            val elfPositions = smallSample.toElfPositions()
+            val newElfPositions = elfPositions.newElfPositions(0)
+            newElfPositions.contains(ElfPosition(2,0)) shouldBe true
+            newElfPositions.contains(ElfPosition(3,0)) shouldBe true
+            newElfPositions.contains(ElfPosition(2,2)) shouldBe true
+            newElfPositions.contains(ElfPosition(3,3)) shouldBe true
+            newElfPositions.contains(ElfPosition(2,4)) shouldBe true
+        }
+        "process small sample data over 10 rounds" {
+            val elfPositions = smallSample.toElfPositions()
+            val newElfPositions = elfPositions.moveElves(10)
+            newElfPositions.contains(ElfPosition(2, 0)) shouldBe true
+            newElfPositions.contains(ElfPosition(4, 1)) shouldBe true
+            newElfPositions.contains(ElfPosition(0, 2)) shouldBe true
+            newElfPositions.contains(ElfPosition(4, 3)) shouldBe true
+            newElfPositions.contains(ElfPosition(2, 5)) shouldBe true
+        }
+        "process large sample data over 10 rounds" {
+            val elfPositions = sampleInput.toElfPositions()
+            val newElfPositions = elfPositions.moveElves(10)
+            newElfPositions.toString(0) shouldBe "......#....."
+            newElfPositions.toString(1) shouldBe "..........#."
+            newElfPositions.toString(2) shouldBe ".#.#..#....."
+            newElfPositions.toString(3) shouldBe ".....#......"
+            newElfPositions.toString(4) shouldBe "..#.....#..#"
+            newElfPositions.toString(5) shouldBe "#......##..."
+            newElfPositions.toString(6) shouldBe "....##......"
+            newElfPositions.toString(7) shouldBe ".#........#."
+            newElfPositions.toString(8) shouldBe "...#.#..#..."
+            newElfPositions.toString(9) shouldBe "............"
+            newElfPositions.toString(10) shouldBe "...#..#..#.."
+        }
+        "partone with sample input is 110" {
+            partOne(sampleInput) shouldBe 110
+        }
+        "partone with puzzle input is 3906" {
+            partOne(puzzleInput) shouldBe 3906
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two with sample input is 20" {
+            partTwo(sampleInput) shouldBe 20
+        }
+        "parttwo with puzzle input is 895" {
+            partTwo(puzzleInput) shouldBe 895
         }
     })
 })
+
+fun ElfPositions.toString(row:Int) =
+    (minOf { it.x }..maxOf { it.x }).joinToString("") { if (contains(ElfPosition(it, row))) "#" else "." }
+
