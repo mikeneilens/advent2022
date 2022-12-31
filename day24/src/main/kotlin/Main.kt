3728163010daaefa0337b9c98a84dc92053795d7
Mike Neilens <mike.neilens@gmail.com>
2022-12-31 15:13:47 +0000

    day24

--- day24/src/main/kotlin/Main.kt
+++ day24/src/main/kotlin/Main.kt
@@ -1,1 +1,122 @@
-fun helloWorld() = "hello world"
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+fun partOne(input: List<String>): Int {
+    val valley = parse(input)
+    val initialState = State(valley.start, 0)
+    val final = aStar(initialState, valley, valley.goal)
+    return final.time
+}
+
+fun partTwo(input: List<String>): Int {
+    val valley = parse(input)
+    val atTheStart = State(valley.start, 0)
+    val atTheEnd = aStar(atTheStart, valley, valley.goal)
+    val atTheStartAgain = aStar(atTheEnd, valley, valley.start)
+    val atTheEndAgain = aStar(atTheStartAgain, valley, valley.goal)
+    return atTheEndAgain.time
+}
+
+data class Position(val x: Int, val y: Int)  {
+    val manhattanDistance = abs(x) + abs(y)
+
+    operator fun plus(other: Position) = Position(x + other.x,y + other.y)
+
+    operator fun minus(other: Position) = Position(x - other.x, y - other.y)
+
+    companion object {
+        private val up = Position(0,-1)
+        private val down = Position(0,1)
+        private val left = Position(-1,0)
+        private val right = Position(1,0)
+        private val none = Position(0,0)
+
+        val allDirections = listOf(up, down, left, right, none)
+    }
+}
+
+fun aStar(initialState: State, valley: Valley, goal: Position): State {
+    val cameFrom = mutableMapOf<State, State>()
+    val costSoFar = mutableMapOf(initialState to initialState.time)
+    fun estimatedCostToGoal(state: State) = (valley.goal - state.location).manhattanDistance
+    val frontier = PriorityQueue(compareBy<State> { costSoFar.getValue(it) + estimatedCostToGoal(it) })
+    frontier.add(initialState)
+
+    while (frontier.isNotEmpty()) {
+        val current = frontier.poll()
+
+        if (current.location == goal) return current
+
+        valley.nextStates(current).forEach { next ->
+            val newCost = costSoFar.getValue(current) + 1
+            if (newCost < costSoFar.getOrDefault(next, Int.MAX_VALUE)) {
+                costSoFar[next] = newCost
+                frontier.add(next)
+                cameFrom[next] = current
+            }
+        }
+    }
+
+    throw Exception("Frontier was empty before goal was reached")
+}
+
+data class State(val location: Position, val time: Int)
+
+interface Flake {
+    val x:Int
+    val y:Int
+    fun after(time:Int):Position
+}
+data class UpFlake(override val x:Int, override val y:Int, val size:Int):Flake {
+    override fun after(time: Int) = Position(x, ((y - time.mod(size)) - 1).mod(size) + 1)
+}
+data class DownFlake(override val x:Int, override val y:Int, val size:Int):Flake {
+    override fun after(time: Int) = Position(x, ((y + time.mod(size)) - 1).mod(size) + 1)
+}
+data class LeftFlake(override val x:Int, override val y:Int, val size:Int):Flake {
+    override fun after(time: Int) = Position( ((x - time.mod(size)) - 1).mod(size) + 1, y)
+}
+data class RightFlake(override val x:Int, override val y:Int, val size:Int):Flake {
+    override fun after(time: Int) = Position( ((x + time.mod(size)) - 1).mod(size) + 1, y)
+}
+
+class Valley(
+    val flakesOnRow: List<List<Flake>>,
+    val flakesOnCol: List<List<Flake>>,
+    private val xBounds:IntRange,
+    private val yBounds:IntRange
+) {
+    val start = Position(xBounds.first, yBounds.first - 1)
+    val goal = Position(xBounds.last, yBounds.last + 1)
+
+    fun nextStates(state: State): List<State> {
+        return Position.allDirections.map {direction ->  state.location + direction }
+            .filter { position ->
+                (position.isWithinBounds() || position == start || position == goal) && !positionContainsSnow(position, state.time + 1 )  //position !in getSnow(state.time + 1).keys
+            }
+            .map { State(it, state.time + 1) }
+    }
+
+    private fun positionContainsSnow(position:Position, time:Int) =
+        position.y < flakesOnRow.size && position.x < flakesOnCol.size &&
+                (flakesOnRow[position.y].map{it.after(time)}.any{ it == position} || flakesOnCol[position.x].map{it.after(time)}.any{ it == position})
+
+    private fun Position.isWithinBounds() = (x in xBounds && y in yBounds)
+}
+
+fun parse(input: List<String>): Valley {
+    val flakes = input.flatMapIndexed { y, line ->
+        line.mapIndexedNotNull { x, char ->
+            when (char) {
+                '<' -> LeftFlake(x, y, line.length - 2)
+                '>' -> RightFlake(x, y, line.length - 2)
+                '^' -> UpFlake(x, y, input.size - 2)
+                'v' -> DownFlake(x, y, input.size - 2)
+                else -> null
+            }
+        }
+    }
+    val flakesOnRow =(0..(input.size - 2)).map { y -> flakes.filter { it.y == y } }
+    val flakesOnCol = listOf<List<Flake>>() + (0..(input.first().length - 2)).map { x -> flakes.filter { it.x == x } }
+    return Valley(flakesOnRow, flakesOnCol, 1..input.first().length -2, 1..input.size -2 )
+}
--- day24/src/test/kotlin/MainTest.kt
+++ day24/src/test/kotlin/MainTest.kt
@@ -3,13 +3,36 @@ import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "parsing sample input into snowflakes for each row" {
+            val flakesForRow = parse(sampleInput).flakesOnRow
+            flakesForRow[1].size shouldBe 5
+            flakesForRow[2].size shouldBe 3
+            flakesForRow[3].size shouldBe 5
+            flakesForRow[4].size shouldBe 6
+        }
+        "parsing sample input into snowflakes for each columnn" {
+            val flakesForColumn = parse(sampleInput).flakesOnCol
+            flakesForColumn[1].size shouldBe 3
+            flakesForColumn[2].size shouldBe 4
+            flakesForColumn[3].size shouldBe 1
+            flakesForColumn[4].size shouldBe 3
+            flakesForColumn[5].size shouldBe 4
+            flakesForColumn[6].size shouldBe 4
+        }
+        "part one with sample input" {
+            partOne(sampleInput) shouldBe 18
+        }
+        "part one with puzzle input is 288" {
+            partOne(puzzleInput) shouldBe 288
         }
     })
     "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "part two with sample input is 54" {
+            partTwo(sampleInput) shouldBe 54
         }
+        "part two with puzzle input is 861" {
+            partTwo(puzzleInput) shouldBe 861
+        }
+
     })
 })
