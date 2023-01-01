e94613a5177e540255216d1008e313f748d37441
Mike Neilens <mike.neilens@gmail.com>
2023-01-01 20:42:59 +0000

    day25

--- day25/src/main/kotlin/Main.kt
+++ day25/src/main/kotlin/Main.kt
@@ -1,1 +1,29 @@
-fun helloWorld() = "hello world"
+import kotlin.math.pow
+
+val powerOfFive = (0..20).map{5.0.pow(it).toLong()}
+
+fun String.snafuToDecimalNumber() = reversed().mapIndexed { i, n -> n.snafuDigitToDecimal() * powerOfFive[i]}.sum()
+
+fun Char.snafuDigitToDecimal() = if (this == '=') -2L else if (this == '-') -1L else this.toString().toLong()
+fun Long.decimalToSnafuDigit() =  if (this >= 0L) this.toString() else if (this == -1L) "-" else "="
+
+fun Long.decimalNumberToSnafu() = toBase5(powerOfFive.reversed()).correctDigits().toSnafuString()
+
+fun Long.toBase5(powerOfFive:List<Long>, result:List<Long> = emptyList()):List<Long> =
+    if (powerOfFive.isEmpty()) result
+    else (this % powerOfFive.first()).toBase5(powerOfFive.drop(1), result + this / powerOfFive.first())
+
+fun List<Long>.correctDigits():List<Long> {
+    val snafuDigits = this.toMutableList()
+    indices.forEach { i ->
+        if (snafuDigits[i] in 3L..5L) {
+            snafuDigits[i - 1] = snafuDigits[i - 1] + 1
+            snafuDigits[i] =  snafuDigits[i] - 5L
+        }
+    }
+    return if (snafuDigits.toList() == this) this else snafuDigits.correctDigits()
+}
+
+fun List<Long>.toSnafuString() = joinToString("",transform = Long::decimalToSnafuDigit).trim { it == '0' }
+
+fun partOne(data:List<String>):String = data.sumOf(String::snafuToDecimalNumber).decimalNumberToSnafu()
--- day25/src/test/kotlin/MainTest.kt
+++ day25/src/test/kotlin/MainTest.kt
@@ -2,14 +2,42 @@ import io.kotest.core.spec.style.WordSpec
 import io.kotest.matchers.shouldBe
 
 class MainTest: WordSpec( {
+    val sampleSNAFU = """
+1=-0-2,1747
+12111,906
+2=0=,198
+21,11
+2=01,201
+111,31
+20012,1257
+112,32
+1=-1=,353
+1-12,107
+12,7
+1=,3
+122,37
+""".trimIndent().split("\n")
     "Part one" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
+        "converting SNAFU to decimal" {
+            sampleSNAFU.forEach { line ->
+                val snafu = line.split(",")[0]
+                val number = line.split(",")[1].toLong()
+                snafu.snafuToDecimalNumber() shouldBe number
+            }
+        }
+        "convert a number to a base 5 number" {
+            55L.toBase5(powerOfFive.reversed()).dropWhile { it == 0L } shouldBe listOf(2L,1L,0L) // i.e. 210
+        }
+        "convert a decimal number to SNAFU" {
+            sampleSNAFU.forEach { line ->
+                val snafu = line.split(",")[0]
+                val number = line.split(",")[1].toLong()
+                number.decimalNumberToSnafu() shouldBe snafu
+            }
+        }
+        "part one with puzzle input gives 2-1-110-=01-1-0-0==2" {
+            partOne(puzzleInput) shouldBe "2-1-110-=01-1-0-0==2"
         }
     })
-    "Part two" should ({
-        "test" {
-            helloWorld() shouldBe "hello world"
-        }
-    })
+
 })
